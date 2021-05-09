cec314202dd8d9424c5db31386a238773ee5d1de
james.lee <james.lee@devncore.org>
2021-05-09 21:51:37 +0900

    Project structure

---
+++ src/Leagueoflegends.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leagueoflegends.Data
+{
+    public class Class1
+    {
+    }
+}
