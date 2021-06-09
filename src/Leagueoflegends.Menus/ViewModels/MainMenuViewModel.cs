2329aea0b46fa813404dd306f930350d6f0f2582
Yejin Kim <elena.kim@devncore.org>
2021-06-09 22:19:35 +0900

    Update MainView

--- src/Leagueoflegends.Menus/ViewModels/MainMenuViewModel.cs
+++ src/Leagueoflegends.Menus/ViewModels/MainMenuViewModel.cs
@@ -1,22 +1,42 @@
 ﻿using Leagueoflegends.Data.Menu;
 using Leagueoflegends.Windowbase.Mvvm;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Leagueoflegends.Menus.ViewModels
 {
 	public class MainMenuViewModel : ObservableObject
 	{
-		public List<MenuBoxModel> Menus { get; }
+		#region Menus
 
-		public MainMenuViewModel(RelayCommand<MenuBoxModel> menuCommand)
+		private List<MenuBoxModel> _menus;
+		public List<MenuBoxModel> Menus
+		{
+            get { return _menus; }
+            set { _menus = value; OnPropertyChanged(); }
+		}
+
+		private MenuBoxModel _currentMenu;
+		public MenuBoxModel CurrentMenu
+		{
+			get { return _currentMenu; }
+			set { _currentMenu = value; OnPropertyChanged(); }
+		}
+        #endregion
+
+        #region Constructor
+
+        public MainMenuViewModel(RelayCommand<MenuBoxModel> menuCommand)
 		{
 			Menus = GetMenus();
 			Menus.ForEach(x => x.MenuSelectCommand = menuCommand);
+			CurrentMenu = Menus.First();
 		}
+        #endregion
 
-		#region GetMenus
+        #region GetMenus
 
-		private List<MenuBoxModel> GetMenus()
+        private List<MenuBoxModel> GetMenus()
 		{
 			List<MenuBoxModel> source = new()
 			{
