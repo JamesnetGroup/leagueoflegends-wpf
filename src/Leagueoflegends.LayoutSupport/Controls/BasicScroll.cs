618d0b480f0a5f017309f724884886639a8878f5
kyj <elena.kim@devncore.org>
2021-06-17 14:40:30 +0900

    Update ScrollViewer

--- src/Leagueoflegends.LayoutSupport/Controls/BasicScroll.cs
+++ src/Leagueoflegends.LayoutSupport/Controls/BasicScroll.cs
@@ -1,8 +1,16 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Leagueoflegends.LayoutSupport.Controls
 {
 	public class BasicScroll : ScrollViewer
-	{
-	}
+    {
+        #region DefaultStyleKey
+
+        static BasicScroll()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BasicScroll), new FrameworkPropertyMetadata(typeof(BasicScroll)));
+        }
+        #endregion
+    }
 }
