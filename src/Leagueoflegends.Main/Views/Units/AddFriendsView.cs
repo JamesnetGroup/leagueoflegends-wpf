8a33997797ccb9921bd24130f163be94b22d9275
kyj <elena.kim@devncore.org>
2021-06-18 15:15:47 +0900

    Update AddFriendsView

---
+++ src/Leagueoflegends.Main/Views/Units/AddFriendsView.cs
@@ -0,0 +1,13 @@
+﻿using Leagueoflegends.LayoutSupport.Controls;
+using System.Windows;
+
+namespace Leagueoflegends.Main.Views
+{
+	public class AddFriendsView : View
+	{
+		static AddFriendsView()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(AddFriendsView), new FrameworkPropertyMetadata(typeof(AddFriendsView)));
+		}
+	}
+}
