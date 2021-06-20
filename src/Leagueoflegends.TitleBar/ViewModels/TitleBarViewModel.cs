cc850d67e3a8087a7d5222728faf838cfb9a0609
james.lee <james.lee@devncore.org>
2021-06-20 22:29:46 +0900

    fix struct

--- src/Leagueoflegends.TitleBar/ViewModels/TitleBarViewModel.cs
+++ src/Leagueoflegends.TitleBar/ViewModels/TitleBarViewModel.cs
@@ -1,15 +1,35 @@
 ﻿using Leagueoflegends.Windowbase.Mvvm;
+using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Leagueoflegends.TitleBar.ViewModels
 {
 	public class TitleBarViewModel : ObservableObject
 	{
-		public ICommand TitleBarSelectCommand { get; }
+		#region Commands
 
-		public TitleBarViewModel(RelayCommand<object> titlebarCommand)
+		public ICommand TitleBarSelectCommand { get; }
+		#endregion
+
+		#region Constructor
+
+		public TitleBarViewModel()
 		{
-			TitleBarSelectCommand = titlebarCommand;
+			TitleBarSelectCommand = new RelayCommand<string>(ButtonClick);
 		}
+		#endregion
+
+		#region ButtonClick
+
+		private void ButtonClick(string tag)
+		{
+			switch (tag)
+			{
+				case "MIN": Window.GetWindow(View).WindowState = WindowState.Minimized; break;
+				case "CLOSE": Environment.Exit(0); break;
+			}
+		}
+		#endregion
 	}
 }
