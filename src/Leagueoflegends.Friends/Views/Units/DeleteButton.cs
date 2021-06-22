893b88b5b0cb615b20d1b25a811abf545994eee8
james.lee <james.lee@devncore.org>
2021-06-22 23:58:08 +0900

    Update

---
+++ src/Leagueoflegends.Friends/Views/Units/DeleteButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Friends.Views
+{
+	public class DeleteButton : Button
+	{
+		#region DefaultStyleKey
+
+		static DeleteButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(DeleteButton), new FrameworkPropertyMetadata(typeof(DeleteButton)));
+		}
+		#endregion
+	}
+}
---
+++ src/Leagueoflegends.Friends/Views/Units/DoneButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Friends.Views
+{
+	public class DoneButton : Button
+	{
+		#region DefaultStyleKey
+
+		static DoneButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(DoneButton), new FrameworkPropertyMetadata(typeof(DoneButton)));
+		}
+		#endregion
+	}
+}
