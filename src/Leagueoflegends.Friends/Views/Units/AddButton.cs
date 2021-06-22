d558da58e3079abe58e0d6611cc784d5751754a0
james.lee <james.lee@devncore.org>
2021-06-22 23:58:08 +0900

    Update

---
+++ src/Leagueoflegends.Friends/Views/Units/AddButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Friends.Views
+{
+	public class AddButton : Button
+	{
+		#region DefaultStyleKey
+
+		static AddButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(AddButton), new FrameworkPropertyMetadata(typeof(AddButton)));
+		}
+		#endregion
+	}
+}
