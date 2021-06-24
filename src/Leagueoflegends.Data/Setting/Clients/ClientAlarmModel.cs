5eb198c2ea28e7673ec34470105af8ee18fcf3a3
kyj <elena.kim@devncore.org>
2021-06-24 11:15:57 +0900

    Add ClientAlarmView

---
+++ src/Leagueoflegends.Data/Setting/Clients/ClientAlarmModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Leagueoflegends.Data.Setting.Clients
+{
+	public class ClientAlarmModel
+	{
+		public bool ESports { get; set; } = true;
+		public bool GameInvitation { get; set; } = true;
+
+		public ClientAlarmModel()
+		{
+		}
+	}
+}
---
+++ src/Leagueoflegends.LayoutSupport/Controls/CheckBox02.cs
@@ -0,0 +1,13 @@
+﻿using Leagueoflegends.Controls;
+using System.Windows;
+
+namespace Leagueoflegends.LayoutSupport.Controls
+{
+	public class CheckBox02 : RiotCheckBox
+    {
+        static CheckBox02()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBox02), new FrameworkPropertyMetadata(typeof(CheckBox02)));
+        }
+    }
+}
