b4b8ca95f22d5f0f97166e617b937b5244f55e75
james.lee <james.lee@devncore.org>
2021-06-24 21:56:32 +0900

    Update

---
+++ src/Leagueoflegends.Main/UI/Units/FriendsGroupExpander.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Leagueoflegends.Main.UI.Units
+{
+	public class FriendsGroupExpander : ToggleButton
+	{
+		#region DefaultStyleKey
+
+		static FriendsGroupExpander()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FriendsGroupExpander), new FrameworkPropertyMetadata(typeof(FriendsGroupExpander)));
+		}
+		#endregion
+	}
+}
