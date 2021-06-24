d79c066a1d73d50ab726985a919affeb22269c99
james.lee <james.lee@devncore.org>
2021-06-24 22:20:50 +0900

    Update

---
+++ src/Leagueoflegends.Main/UI/Units/AvataThumb.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Main.UI.Units
+{
+	public class AvataThumb : Control
+	{
+		#region DefaultStyleKey
+
+		static AvataThumb()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(AvataThumb), new FrameworkPropertyMetadata(typeof(AvataThumb)));
+		}
+		#endregion
+		public AvataThumb()
+		{ 
+		}
+	}
+}
