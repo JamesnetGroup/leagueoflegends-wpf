a8221daaa078482abf50a7f6ca4f5e8e60a05713
james.lee <james.lee@devncore.org>
2021-06-24 21:56:32 +0900

    Update

---
+++ src/Leagueoflegends.Main/UI/Units/FriendsListItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Main.UI.Units
+{
+	public class FriendsListItem : Control
+	{
+		#region DefaultStyleKey
+
+		static FriendsListItem()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FriendsListItem), new FrameworkPropertyMetadata(typeof(FriendsListItem)));
+		}
+		#endregion
+	}
+}
