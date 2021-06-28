844a32f3bde088e4ccf546b5b392e37881256eb1
kyj <elena.kim@devncore.org>
2021-06-28 15:17:30 +0900

    Add ClientBlockView

---
+++ src/Leagueoflegends.Client.Block/UI/ClientBlockView.cs
@@ -0,0 +1,16 @@
+﻿using Leagueoflegends.LayoutSupport.Controls;
+using System.Windows;
+
+namespace Leagueoflegends.Client.Block.UI
+{
+	public class ClientBlockView : View
+    {
+        #region ClientBlockView
+
+        static ClientBlockView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ClientBlockView), new FrameworkPropertyMetadata(typeof(ClientBlockView)));
+        }
+		#endregion
+	}
+}
