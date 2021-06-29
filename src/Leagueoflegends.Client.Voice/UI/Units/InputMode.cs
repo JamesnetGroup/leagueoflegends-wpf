bf709642f7190c61829d9ff70cd021f04490415b
kyj <elena.kim@devncore.org>
2021-06-29 11:51:27 +0900

    Change structure

--- src/Leagueoflegends.Client.Voice/UI/Units/InputMode.cs
+++ src/Leagueoflegends.Client.Voice/UI/Units/InputMode.cs
@@ -1,9 +1,9 @@
-﻿using Leagueoflegends.LayoutSupport.Controls;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Leagueoflegends.Client.Voice.UI.Units
 {
-	public class InputMode : View
+	public class InputMode : Control
     {
         #region DefaultStyleKey
 
