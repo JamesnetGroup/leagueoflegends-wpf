d58a09597afe1d2b3d32a26b463e33f1db3afff0
kyj <elena.kim@devncore.org>
2021-06-29 10:02:22 +0900

    Update GeneralView

---
+++ src/Leagueoflegends.Home.General/UI/Units/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using Leagueoflegends.LayoutSupport.Controls;
+using System.Windows;
+
+namespace Leagueoflegends.Home.General.UI.Units
+{
+	public class Rotation : View
+    {
+        static Rotation()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Rotation), new FrameworkPropertyMetadata(typeof(Rotation)));
+        }
+    }
+}
