d7840c71a73c69d8ed0164b72d90666d86db86be
kyj <elena.kim@devncore.org>
2021-07-02 16:41:43 +0900

    Add Project

---
+++ src/Leagueoflegends.Game.Sound/GameSoundView.cs
@@ -0,0 +1,13 @@
+﻿using Leagueoflegends.LayoutSupport.Controls;
+using System.Windows;
+
+namespace Leagueoflegends.Game.Sound
+{
+	public class GameSoundView : View
+    {
+        static GameSoundView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GameSoundView), new FrameworkPropertyMetadata(typeof(GameSoundView)));
+        }
+    }
+}
