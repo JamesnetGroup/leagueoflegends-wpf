45785ba37bb8690d80388a8870c85feb0e574de0
james.lee <james.lee@devncore.org>
2021-07-06 00:15:35 +0900

    Update

---
+++ src/Leagueoflegends.DBEntity/Local/Api/GetInputMode.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Leagueoflegends.DBEntity.Local.Entities;
+using Leagueoflegends.DBEntity.Local.Entities.Extend;
+
+namespace Leagueoflegends.DBEntity.Local.Api
+{
+	public class GetInputMode
+	{
+		public List<RequestUsers> Run(int mySeq)
+		{
+			using RiotContext db = new();
+			var users = db.Users
+				.Where(x => x.Seq != mySeq)
+				.Select(x => new RequestUsers(x))
+				.ToList();
+
+			return users;
+		}
+	}
+}
