a55420f98f294334123cb59086a9e32975d9ef86
kyj <elena.kim@devncore.org>
2021-07-30 09:20:24 +0900

    organizing structure

---
+++ src/Leagueoflegends.Settings.InGame/UI/Units/Game/Play.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Settings.InGame.UI.Units
+{
+	public class Play : Control
+    {
+        static Play()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Play), new FrameworkPropertyMetadata(typeof(Play)));
+        }
+    }
+}
