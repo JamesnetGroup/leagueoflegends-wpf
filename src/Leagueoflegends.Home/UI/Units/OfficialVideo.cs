9178936ff7ba01b458a1f49ebbcfb1a6fb5cbf05
devncore-elena <elena.kim@devncore.org>
2021-08-10 10:44:12 +0900

    fix structure

---
+++ src/Leagueoflegends.Home/UI/Units/OfficialVideo.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Home.UI.Units
+{
+    public class OfficialVideo : Control
+    {
+        #region DefaultStyleKey
+
+        static OfficialVideo()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(OfficialVideo), new FrameworkPropertyMetadata(typeof(OfficialVideo)));
+        }
+		#endregion
+	}
+}
