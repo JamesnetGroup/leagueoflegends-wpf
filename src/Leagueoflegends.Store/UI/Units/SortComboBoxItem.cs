982e8a615ea3edf4b509e2a1cfb09c2c35705346
devncore-elena <elena.kim@devncore.org>
2021-08-12 14:33:20 +0900

    update storeview

---
+++ src/Leagueoflegends.Store/UI/Units/SortComboBoxItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Leagueoflegends.Store.UI.Units
+{
+	public class SortComboBoxItem : ComboBoxItem
+    {
+        #region DefaultStyleKey
+
+        static SortComboBoxItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SortComboBoxItem), new FrameworkPropertyMetadata(typeof(SortComboBoxItem)));
+        }
+		#endregion
+	}
+}
