0681229dbb4f9bbe6872dcb05cc7a051d1467f4a
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-19 06:29:41 +0900

    c

---
+++ src/Leagueoflegends.Clash/UI/Units/Manual.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Leagueoflegends.Clash.UI.Units
+{
+    public class Manual : Control
+    {
+        static Manual()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Manual), new FrameworkPropertyMetadata(typeof(Manual)));
+        }
+    }
+}
