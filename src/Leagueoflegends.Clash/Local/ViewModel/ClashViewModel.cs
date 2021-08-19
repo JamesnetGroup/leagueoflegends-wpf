d85a9f526e4da7df9da933d22625a186bbced336
devncore-elena <elena.kim@devncore.org>
2021-08-19 17:57:46 +0900

    Update RiotButton

--- src/Leagueoflegends.Clash/Local/ViewModel/ClashViewModel.cs
+++ src/Leagueoflegends.Clash/Local/ViewModel/ClashViewModel.cs
@@ -1,13 +1,8 @@
 ﻿using Leagueoflegends.Foundation.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Leagueoflegends.Clash.Local.ViewModel
 {
-    public class ClashViewModel : ObservableObject
+	public class ClashViewModel : ObservableObject
     {
         #region Constructor
 
--- src/Leagueoflegends.LayoutSupport/Controls/RiotButton.cs
+++ src/Leagueoflegends.LayoutSupport/Controls/RiotButton.cs
@@ -12,5 +12,20 @@ namespace Leagueoflegends.LayoutSupport.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(RiotButton), new FrameworkPropertyMetadata(typeof(RiotButton)));
         }
         #endregion
-    }
+
+        #region DependencyProperty
+
+        public static readonly DependencyProperty ContentMarginProperty = DependencyProperty.Register(
+            "ContentMargin", typeof(Thickness), typeof(RiotButton), new PropertyMetadata(default(Thickness)));
+        #endregion
+
+        #region ContentMargin
+
+        public Thickness ContentMargin
+        {
+            get { return (Thickness)GetValue(ContentMarginProperty); }
+            set { SetValue(ContentMarginProperty, value); }
+        }
+		#endregion
+	}
 }
