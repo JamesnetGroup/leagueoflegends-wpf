d092c4ba4c6b4259fd8b48937a1d48ec655442b3
devncore-elena <elena.kim@devncore.org>
2021-08-20 09:40:16 +0900

    update submenu order and fix friends

--- src/Leagueoflegends.Clash/UI/Units/CityCup.cs
+++ src/Leagueoflegends.Clash/UI/Units/CityCup.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Leagueoflegends.Clash.UI.Units
 {
-    public class CityCup : Control
+	public class CityCup : Control
     {
-        #region Constructor
+        #region DefaultStyleKey
 
         static CityCup()
         {
