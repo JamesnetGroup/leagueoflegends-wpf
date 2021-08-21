0760736652d744fa34a894df84934e2e35d30a2a
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-21 19:17:07 +0900

    Free Player Update

--- src/Lol.Controls/Primitives/Icon.cs
+++ src/Lol.Controls/Primitives/Icon.cs
@@ -74,6 +74,7 @@ namespace Lol.Controls.Primitives
                 case GeoIcon.Plus: icon.Data = IconData.PLUS.ToGeometry(); break;
                 case GeoIcon.MenuLeft: icon.Data = IconData.MENU_LEFT.ToGeometry(); break;
                 case GeoIcon.Square: icon.Data = IconData.SQUARE.ToGeometry(); break;
+                case GeoIcon.AccountSupervisor: icon.Data = IconData.ACCOUNT_SUPERVISOR.ToGeometry(); break;
 
                 case GeoIcon.None:
                     break;
