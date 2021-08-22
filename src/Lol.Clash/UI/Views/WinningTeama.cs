252c793e5c1a9ea70a2ecfee3e89005c5dd6205e
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-23 06:29:59 +0900

    WinningTeam View Connection

--- src/Lol.Clash/UI/Views/WinningTeama.cs
+++ src/Lol.Clash/UI/Views/WinningTeama.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lol.LayoutSupport.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ using System.Windows.Shapes;
 
 namespace Lol.Clash.UI.Views
 {
-    public class WinningTeama : Control
+    public class WinningTeama : View
     {
         static WinningTeama()
         {
