916ec37fb7887eb78d1d1f6063bdf8008aeb702f
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-25 05:22:12 +0900

    Update

---
+++ src/Lol.Clash/UI/Units/CupListBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Clash.UI.Units
+{
+
+    public class CupListBox : ListBox
+    {
+        static CupListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CupListBox), new FrameworkPropertyMetadata(typeof(CupListBox)));
+        }
+    }
+}
