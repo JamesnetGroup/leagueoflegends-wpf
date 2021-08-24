5d258c8893779aa64712413b1fb30bfab8a1cfd7
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-25 05:33:54 +0900

    Update

---
+++ src/Lol.Clash/Local/ViewModel/WinningTeamViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Lol.Foundation.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.Clash.Local.ViewModel
+{
+    public class WinningTeamViewModel : ObservableObject
+    {
+    }
+}
