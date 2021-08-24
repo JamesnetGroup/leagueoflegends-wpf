b2212910b8f9c5344baba5da952c9bcce0913689
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-25 05:47:02 +0900

    Update

--- src/Lol.Clash/Local/ViewModel/WinningTeamViewModel.cs
+++ src/Lol.Clash/Local/ViewModel/WinningTeamViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Lol.Foundation.Mvvm;
+﻿using Lol.Data.WinningTeam;
+using Lol.ExampleData.WinningTeam;
+using Lol.Foundation.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,19 @@ namespace Lol.Clash.Local.ViewModel
 {
     public class WinningTeamViewModel : ObservableObject
     {
+        private List<CupModel> _cups;
+
+        #region Cups
+
+        public List<CupModel> Cups
+        {
+            get => _cups;
+            set { _cups = value; OnPropertyChanged(); }
+        }
+        #endregion
+        public WinningTeamViewModel()
+        {
+            Cups = ExamWinningTeam.GetCup();
+        }
     }
 }
