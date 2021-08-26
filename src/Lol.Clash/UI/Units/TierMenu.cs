e2b769c0d9a1e9e5dd95d248d01151a9ea9e28fe
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-27 06:00:02 +0900

    TierMenu Add

---
+++ src/Lol.Clash/UI/Units/TierMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Lol.Clash.UI.Units
+{
+    #region DefaultStyleKey
+
+    public class TierMenu : ListBox
+    {
+        static TierMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TierMenu), new FrameworkPropertyMetadata(typeof(TierMenu)));
+        }
+    }
+    #endregion
+}
