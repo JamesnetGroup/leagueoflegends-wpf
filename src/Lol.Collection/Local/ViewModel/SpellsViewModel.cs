e02e4aee0a699a1da0edc47ff3657e42c33633a0
nuhabo <nuhabo03@gmail.com>
2021-08-26 20:57:34 +0900

    'update'

---
+++ src/Lol.Collection/Local/ViewModel/SpellsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Lol.Foundation.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.Collection.Local.ViewModel
+{
+    public class SpellsViewModel : ObservableObject
+    {
+        public SpellsViewModel()
+        {
+
+        }
+    }
+}
