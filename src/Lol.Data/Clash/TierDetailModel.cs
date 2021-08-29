ca546433d8c2824f4e406c809b40a1108527389d
james lee <james.lee@devncore.org>
2021-08-29 21:18:26 +0900

    Update

--- src/Lol.Data/Clash/TierDetailModel.cs
+++ src/Lol.Data/Clash/TierDetailModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lol.Data.Clash
+﻿namespace Lol.Data.Clash
 {
 	public class TierDetailModel
 	{
