d2018e24f32b2a112fe5da26ae5a223d11fd04c6
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-29 14:35:04 +0900

    Image Add

--- src/Lol.Data/Clash/TierDetailModel.cs
+++ src/Lol.Data/Clash/TierDetailModel.cs
@@ -12,13 +12,15 @@ namespace Lol.Data.Clash
 		public string Name { get; set; }
 		public string SubName { get; set; }
 		public string Time { get; set; }
+		public string ImageSource { get; set; }
 
-		public TierDetailModel(int seq, string name, string sub_name, string time)
+		public TierDetailModel(int seq, string name, string sub_name, string time, string image_source)
 		{
 			Seq = seq;
 			Name = name;
 			SubName = sub_name;
 			Time = time;
+			ImageSource = image_source;
 		}
 	}
 }
