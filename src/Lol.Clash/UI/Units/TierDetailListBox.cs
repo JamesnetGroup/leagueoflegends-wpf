2b7f6e8504f86c3a9337b03f099f2ae2321a4898
rlagurdls456 <rlagurdls456@outlook.com>
2021-08-29 13:01:01 +0900

    Add TierDetailListBox

---
+++ src/Lol.Clash/UI/Units/TierDetailListBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Lol.Clash.UI.Units
+{
+    public class TierDetailListBox : ListBox
+    {
+        #region DefaultStyleKey
+
+        static TierDetailListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TierDetailListBox), new FrameworkPropertyMetadata(typeof(TierDetailListBox)));
+        }
+        #endregion
+    }
+}
