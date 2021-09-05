f5ba1269bf2156bd1b185d87431d19dded01fc97
rlagurdls456 <rlagurdls456@outlook.com>
2021-09-06 06:29:08 +0900

    Update

---
+++ src/Lol.Profile/UI/Units/HistoryListBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Profile.UI.Units
+{
+    public class HistoryListBox : ListBox
+    {
+        #region DefaultStyleKey
+
+        static HistoryListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HistoryListBox), new FrameworkPropertyMetadata(typeof(HistoryListBox)));
+        }
+        #endregion
+    }
+}
