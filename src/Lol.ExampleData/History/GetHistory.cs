e97adb2982709122f1eb1b8a9d44aba6c14d0d02
rlagurdls456 <rlagurdls456@outlook.com>
2021-09-07 06:10:03 +0900

    Update

---
+++ src/Lol.ExampleData/History/GetHistory.cs
@@ -0,0 +1,25 @@
+﻿using Lol.Data.History;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.ExampleData.History
+{
+	public partial class ExamHistory
+	{
+		public static List<HistoryModel> GetHistory()
+		{
+			List<HistoryModel> source = new()
+			{
+				new HistoryModel(0, "Ⅰ"),
+				new HistoryModel(1, "Ⅱ"),
+				new HistoryModel(2, "Ⅲ"),
+				new HistoryModel(3, "Ⅳ")
+			};
+
+			return source;
+		}
+	}
+}
