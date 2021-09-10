c24064cdf28e0276f1cecdf46b652409114d05a7
devncore-elena <elena.kim@devncore.org>
2021-09-10 21:40:08 +0900

    Change ViewName and Set Background

---
+++ src/Lol.Collection/UI/Views/ChampionsView.cs
@@ -0,0 +1,16 @@
+﻿using Lol.LayoutSupport.Controls;
+using System.Windows;
+
+namespace Lol.Collection.UI.Views
+{
+	public class ChampionsView : View
+    {
+        #region DefaultStyleKey
+
+        static ChampionsView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ChampionsView), new FrameworkPropertyMetadata(typeof(ChampionsView)));
+        }
+		#endregion
+	}
+}
