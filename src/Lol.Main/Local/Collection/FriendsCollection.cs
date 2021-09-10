67d42721bcb06c5861b793f8417c945a1a259214
james lee <james.lee@devncore.org>
2021-09-10 19:15:21 +0900

    Add region

--- src/Lol.Main/Local/Collection/FriendsCollection.cs
+++ src/Lol.Main/Local/Collection/FriendsCollection.cs
@@ -1,18 +1,38 @@
-﻿using Lol.DBEntity.Local.Entities.Extend;
-using Lol.DBEntity.Local.Entities.Implements;
+﻿using System.Linq;
 using System.Collections.Generic;
-using System.Linq;
+using Lol.DBEntity.Local.Entities.Extend;
+using Lol.DBEntity.Local.Entities.Implements;
 
 namespace Lol.Main.Local.Collection
 {
 	public class FriendsCollection : List<IFriendsList>
 	{
+		#region GeneralFriendsCount
+
 		public int GeneralFriendsCount => GeneralList.Children.Count();
+		#endregion
+
+		#region OfflineFriendsCount
+
 		public int OfflineFriendsCount => OfflineList.Children.Count();
+		#endregion
+
+		#region TotalFriendsCount
+
 		public int TotalFriendsCount => GeneralFriendsCount + OfflineFriendsCount;
+		#endregion
+
+		#region GeneralList 
 
 		public FriendsHeader GeneralList { get; }
+		#endregion
+
+		#region OfflineList
+
 		public FriendsHeader OfflineList { get; }
+		#endregion
+
+		#region Constructor
 
 		public FriendsCollection(List<IFriendsList> items)
 		{
@@ -21,5 +41,6 @@ namespace Lol.Main.Local.Collection
 
 			AddRange(items);
 		}
+		#endregion
 	}
 }
