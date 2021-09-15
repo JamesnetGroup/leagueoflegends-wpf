b0cce0d92e6faa4ea39334e4f0f07ee965818e7e
nuhabo <nuhabo03@gmail.com>
2021-09-16 00:40:15 +0900

    update

--- src/Lol.ExampleData/Collection/GetSpell.cs
+++ src/Lol.ExampleData/Collection/GetSpell.cs
@@ -7,15 +7,19 @@ namespace Lol.ExampleData.Collection
     {
         static string imgResource(string name)
         {
-            return $"/Lol.Resources;component/Images/Spell/{name}.jpg";
+            return $"/Lol.Resources;component/Images/Spell/{name}.png";
         }
 
         public static List<SpellModel> GetSpell()
         {
             var source = new List<SpellModel>();
 
-            source.Add(new SpellModel { Name = "점화", ImageSource = imgResource("ignite") });
+            source.Add(new SpellModel { Name = "유체화", ImageSource = imgResource("speed") });
+            source.Add(new SpellModel { Name = "회복", ImageSource = imgResource("heal") });
+            source.Add(new SpellModel { Name = "방어막", ImageSource = imgResource("shield") });
+            source.Add(new SpellModel { Name = "탈진", ImageSource = imgResource("exhaust") });
             source.Add(new SpellModel { Name = "점멸", ImageSource = imgResource("flash") });
+            source.Add(new SpellModel { Name = "점화", ImageSource = imgResource("ignite") });
 
             return source;
         }
