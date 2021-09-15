9b62404a8cbd7dac8db8aaf142f3d4716bd7d6f1
rlagurdls456 <rlagurdls456@outlook.com>
2021-09-15 22:38:06 +0900

    Update

---
+++ src/Lol.ExampleData/Collection/GetSpell.cs
@@ -0,0 +1,27 @@
+﻿using Lol.Data.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.ExampleData.Collection
+{
+    public partial class ExamSpell
+    {
+        static string imgResource(string name)
+        {
+            return $"/Lol.Resources;component/Images/Spell/{name}.jpg";
+        }
+
+        public static List<SpellModel> GetSpell()
+        {
+            var source = new List<SpellModel>();
+
+            source.Add(new SpellModel {Name = "점화", ImageSource = imgResource("ignite") });
+            source.Add(new SpellModel {Name = "점멸", ImageSource = imgResource("flash") });
+
+            return source;
+        }
+    }
+}
