16873f4e3d7955fe22194e62d37bac67c31cebd8
james lee <james.lee@devncore.org>
2021-09-15 23:03:10 +0900

    Update foundation

--- src/Lol.Foundation/Riotbase/RiotApp.cs
+++ src/Lol.Foundation/Riotbase/RiotApp.cs
@@ -6,14 +6,15 @@ namespace Lol.Foundation.Riotbase
 {
     public class RiotApp : Application
     {
+        // TODO: [James] Non-constant fields should not be visible 메시지 해결 필요!
         public static string REPLAY_PATH;
         public static string HIGHLIGHT_PATH;
 
         static RiotApp()
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            REPLAY_PATH = Path.Combine(path, "League of Legends\\Replays");
-            HIGHLIGHT_PATH = Path.Combine(path, "League of Legends\\Highlights");
+            REPLAY_PATH = Path.Combine(path, @"League of Legends\Replays");
+            HIGHLIGHT_PATH = Path.Combine(path, @"League of Legends\Highlights");
         }
     }
 }
