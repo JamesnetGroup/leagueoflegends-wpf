f7464feb8f4a349b109b70476a24465b26ef609d
james lee <james.lee@devncore.org>
2021-09-16 00:24:52 +0900

    Add yamldatabase project

---
+++ src/Lol.YamlDatabase/Todo.cs
@@ -0,0 +1,13 @@
+﻿// TODO: [James] 이 클래스는 작업항목을 적어놓기 위한 임시 파일입니다.
+
+namespace Lol.YamlDatabase
+{
+    
+    class Todo
+    {
+        // TODO: [James] 필요한 데이터를 YAML 형태로 구현 계획입니다.
+        
+        // TODO: [James] Datas 폴더는 임시 규칙입니다. 언제든지 바뀔 수 있음..
+        // TODO: [James] Datas 안에 있는 .yml 파일명은 확정 X
+    }
+}
