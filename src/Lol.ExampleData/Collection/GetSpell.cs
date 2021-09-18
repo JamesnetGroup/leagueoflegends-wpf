f82f8a25d7c34b629c9a4d506d7e5f80f3757bcf
unknown <lucas.park@devncore.org>
2021-09-18 16:16:43 +0900

    update

--- src/Lol.ExampleData/Collection/GetSpell.cs
+++ src/Lol.ExampleData/Collection/GetSpell.cs
@@ -14,12 +14,17 @@ namespace Lol.ExampleData.Collection
         {
             var source = new List<SpellModel>();
 
-            source.Add(new SpellModel { Name = "유체화", ImageSource = imgResource("speed") });
+            source.Add(new SpellModel { Name = "유체화", ImageSource = imgResource("haste") });
             source.Add(new SpellModel { Name = "회복", ImageSource = imgResource("heal") });
-            source.Add(new SpellModel { Name = "방어막", ImageSource = imgResource("shield") });
+            source.Add(new SpellModel { Name = "방어막", ImageSource = imgResource("barrier") });
             source.Add(new SpellModel { Name = "탈진", ImageSource = imgResource("exhaust") });
+            source.Add(new SpellModel { Name = "총명", ImageSource = imgResource("mana") });
+            source.Add(new SpellModel { Name = "표식", ImageSource = imgResource("snowball") });
+            source.Add(new SpellModel { Name = "순간이동", ImageSource = imgResource("teleport") });
             source.Add(new SpellModel { Name = "점멸", ImageSource = imgResource("flash") });
-            source.Add(new SpellModel { Name = "점화", ImageSource = imgResource("ignite") });
+            source.Add(new SpellModel { Name = "강타", ImageSource = imgResource("smite") });
+            source.Add(new SpellModel { Name = "점화", ImageSource = imgResource("dot") });
+            source.Add(new SpellModel { Name = "정화", ImageSource = imgResource("boost") });
 
             return source;
         }
