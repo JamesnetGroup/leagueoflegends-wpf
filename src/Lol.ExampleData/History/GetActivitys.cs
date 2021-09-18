f23b02f3df85ff49ddf24799f04f39defa5a170d
rlagurdls456 <rlagurdls456@outlook.com>
2021-09-18 18:24:49 +0900

    Update

--- src/Lol.ExampleData/History/GetActivitys.cs
+++ src/Lol.ExampleData/History/GetActivitys.cs
@@ -1,9 +1,5 @@
 ﻿using Lol.Data.History;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lol.ExampleData.History
 {
