19d485811cf8f1ebfe08f1a66bc9b4674283ef51
devncore-elena <elena.kim@devncore.org>
2021-09-18 19:46:27 +0900

    Updtae ProfileView

--- src/Lol.ExampleData/History/GetActivitys.cs
+++ src/Lol.ExampleData/History/GetActivitys.cs
@@ -5,15 +5,16 @@ namespace Lol.ExampleData.History
 {
     public partial class ExamHistory
     {
-        public static List<HistoryModel> GetActivitys()
+        public static List<HistoryModel> GetActivities()
         {
-            List<HistoryModel> source = new List<HistoryModel>();
-
-            source.Add(new HistoryModel {ActivePercent = 10, Job = ""});
-            source.Add(new HistoryModel {ActivePercent = 55, Job = ""});
-            source.Add(new HistoryModel {ActivePercent = 32, Job = ""});
-            source.Add(new HistoryModel {ActivePercent = 64, Job = ""});
-            source.Add(new HistoryModel {ActivePercent = 22, Job = ""});
+            List<HistoryModel> source = new()
+            {
+                new HistoryModel { ActivePercent = 10, Job = "" },
+                new HistoryModel { ActivePercent = 55, Job = "" },
+                new HistoryModel { ActivePercent = 32, Job = "" },
+                new HistoryModel { ActivePercent = 64, Job = "" },
+                new HistoryModel { ActivePercent = 22, Job = "" }
+            };
 
             return source;
         }
