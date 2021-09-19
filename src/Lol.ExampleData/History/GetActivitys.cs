4958edf11aa13d36d732df352a3a7d7fb82fd91a
rlagurdls456 <rlagurdls456@outlook.com>
2021-09-19 11:21:09 +0900

    Update

--- src/Lol.ExampleData/History/GetActivitys.cs
+++ src/Lol.ExampleData/History/GetActivitys.cs
@@ -9,11 +9,12 @@ namespace Lol.ExampleData.History
         {
             List<HistoryModel> source = new()
             {
-                new HistoryModel { ActivePercent = 10, Job = "" },
-                new HistoryModel { ActivePercent = 55, Job = "" },
-                new HistoryModel { ActivePercent = 32, Job = "" },
-                new HistoryModel { ActivePercent = 64, Job = "" },
-                new HistoryModel { ActivePercent = 22, Job = "" }
+                new HistoryModel { ActivePercent = 10, Concept = imgResource("Concepts", "warrior") },
+                new HistoryModel { ActivePercent = 55, Concept = imgResource("Concepts", "tanker") },
+                new HistoryModel { ActivePercent = 32, Concept = imgResource("Concepts", "wizard") },
+                new HistoryModel { ActivePercent = 64, Concept = imgResource("Concepts", "assassin") },
+                new HistoryModel { ActivePercent = 22, Concept = imgResource("Concepts", "support") },
+                new HistoryModel { ActivePercent = 11, Concept = imgResource("Concepts", "dealer") },
             };
 
             return source;
