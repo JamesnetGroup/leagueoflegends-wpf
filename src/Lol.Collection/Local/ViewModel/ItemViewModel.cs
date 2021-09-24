e5fc870f2356880f9bc54fb57c1c3946b17ae3fd
nuhabo <nuhabo03@gmail.com>
2021-09-25 03:57:15 +0900

    update

--- src/Lol.Collection/Local/ViewModel/ItemViewModel.cs
+++ src/Lol.Collection/Local/ViewModel/ItemViewModel.cs
@@ -1,8 +1,24 @@
-﻿using Lol.Foundation.Mvvm;
+﻿using Lol.Data.Collection;
+using Lol.ExampleData.Collection;
+using Lol.Foundation.Mvvm;
+using System.Collections.Generic;
 
 namespace Lol.Collection.Local.ViewModel
 {
     public class ItemViewModel : ObservableObject
     {
+        #region ItemLists
+
+        public List<ItemListModel> ItemLists { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ItemViewModel()
+        {
+            ItemLists = ExamItemList.GetItemList();
+        }
+        #endregion
+
     }
 }
