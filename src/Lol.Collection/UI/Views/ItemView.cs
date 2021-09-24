b5e8ae22afc0626ed93ada2d9ad6591b35772108
devncore-elena <elena.kim@devncore.org>
2021-09-24 11:09:22 +0900

    Clean Project

--- src/Lol.Collection/UI/Views/ItemView.cs
+++ src/Lol.Collection/UI/Views/ItemView.cs
@@ -1,26 +1,16 @@
 ﻿using Lol.LayoutSupport.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lol.Collection.UI.Views
 {
     public class ItemView : View
     {
+        #region DefaultStyleKey
+
         static ItemView()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ItemView), new FrameworkPropertyMetadata(typeof(ItemView)));
         }
+        #endregion
     }
 }
