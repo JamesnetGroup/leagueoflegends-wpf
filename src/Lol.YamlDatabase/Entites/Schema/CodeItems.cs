ffdf2de179dff977d1526107a1008ce4f5b7b1ff
unknown <james.lee@devncore.org>
2021-09-25 07:40:17 -0700

    Update codeitems

---
+++ src/Lol.YamlDatabase/Entites/Schema/CodeItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.YamlDatabase.Entites.Schema
+{
+    public class CodeItems
+    {
+        public int Seq { get; set; }
+        public string Id { get; set; }
+        public string CodeId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemValue { get; set; }
+        public string Explain { get; set; }
+        public int Sort { get; set; }
+    }
+}
