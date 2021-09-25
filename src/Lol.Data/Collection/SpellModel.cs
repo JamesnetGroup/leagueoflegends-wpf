7a7b82af76c6bf96ff4c200a08b300cd2ae07d43
devncore-elena <elena.kim@devncore.org>
2021-09-25 19:44:04 +0900

    Update SpellModel

--- src/Lol.Data/Collection/SpellModel.cs
+++ src/Lol.Data/Collection/SpellModel.cs
@@ -1,13 +1,34 @@
-﻿namespace Lol.Data.Collection
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lol.Data.Collection
 {
     public class SpellModel
     {
         public int Seq { get; set; }
         public string Name { get; set; }
         public int Level { get; set; }
-        public string Maps { get; set; }
+        public List<GamePlayModel> PlayTypes { get; set; }
         public string Content { get; set; }
         public int Delay { get; set; }
+        public string DisplayName => string.Join(", ", PlayTypes.Select(x => x.Name.ToString()).ToArray());
+    }
 
+    public class GamePlayModel
+    {
+        public int Seq { get; set; }
+        public string Name { get; set; }
+
+        public GamePlayModel(int seq)
+        {
+            Seq = seq;
+            Name = seq switch
+            {
+                0 => "무작위 총력전",
+                1 => "클래식",
+                2 => "튜토리얼",
+                _ => throw new System.NotImplementedException(),
+            };
+        }
     }
 }
