9bf9a8a79cc012a418764b5e33dccb278c6ce2a4
devncore-elena <elena.kim@devncore.org>
2021-09-25 21:53:31 +0900

    Update YamlDatabase

--- src/Lol.DBEntity/Local/Entities/Extend/MyFriends.cs
+++ src/Lol.DBEntity/Local/Entities/Extend/MyFriends.cs
@@ -1,4 +1,5 @@
 ﻿using Lol.DBEntity.Common;
+using Lol.DBEntity.Local.Api;
 using Lol.DBEntity.Local.Entities.Implements;
 using Lol.DBEntity.Local.Entities.Schema;
 using System;
@@ -7,11 +8,22 @@ namespace Lol.DBEntity.Local.Entities.Extend
 {
     public class MyFriends : Users, IFriendsList
     {
+        private UsersModel usersModel;
+
         public Type Type => GetType();
 
         public int Status { get; set; }
 
         public MyFriends(Users user)
+        {
+            Seq = user.Seq;
+            Name = user.Name;
+            Created = user.Created;
+            Updated = user.Updated;
+            Status = RandomHelper.Next(0, 4);
+        }
+
+        public MyFriends(UsersModel user)
         {
             Seq = user.Seq;
             Name = user.Name;
