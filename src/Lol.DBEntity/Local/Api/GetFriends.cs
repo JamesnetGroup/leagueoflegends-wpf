2eb178dcf85d2f8cebacbb6a8fe12aa185d5a0b8
devncore-elena <elena.kim@devncore.org>
2021-09-25 21:53:31 +0900

    Update YamlDatabase

--- src/Lol.DBEntity/Local/Api/GetFriends.cs
+++ src/Lol.DBEntity/Local/Api/GetFriends.cs
@@ -1,8 +1,12 @@
 ﻿using Lol.DBEntity.Local.Entities;
 using Lol.DBEntity.Local.Entities.Extend;
 using Lol.DBEntity.Local.Entities.Implements;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace Lol.DBEntity.Local.Api
 {
@@ -10,15 +14,11 @@ namespace Lol.DBEntity.Local.Api
     {
         public List<IFriendsList> Run(int mySeq)
         {
-            using RiotContext db = new();
+            // TODO: [Elena] Yaml DB 임시 작업중!
+            var friends1 = GetFriends1().Where(x=>x.UserSeq == mySeq).ToList();
+            var users1 = GetUsers();
 
-            var query = from f in db.Friends
-                        where f.UserSeq == mySeq
-                        join u in db.Users
-                        on f.FriendsSeq equals u.Seq
-                        select new MyFriends(u);
-
-            var friends = query.ToList()
+            var friends = friends1.Select(x => new MyFriends(users1.First(u => x.FriendsSeq == u.Seq))).ToList()
                 .OrderBy(x => x.Status)
                 .ToList();
 
@@ -34,5 +34,49 @@ namespace Lol.DBEntity.Local.Api
 
             return list;
         }
+
+        private List<FriendsModel> GetFriends1()
+        {
+            string file = "/datas/friends.yml";
+            string path = Environment.CurrentDirectory + file;
+
+            string readText = File.ReadAllText(path);
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            return deserializer.Deserialize<List<FriendsModel>>(readText);
+        }
+
+        private List<UsersModel> GetUsers()
+        {
+            string file = "/datas/users.yml";
+            string path = Environment.CurrentDirectory + file;
+
+            string readText = File.ReadAllText(path);
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            return deserializer.Deserialize<List<UsersModel>>(readText);
+        }
+    }
+    public class UsersModel
+    { 
+        public int Seq { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+    public class FriendsModel
+    {
+        public int Seq { get; set; }
+        public int UserSeq { get; set; }
+        public int FriendsSeq { get; set; }
+        public bool IsAccepted { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
     }
 }
