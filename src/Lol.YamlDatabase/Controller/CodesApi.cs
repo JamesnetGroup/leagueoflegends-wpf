96784899cc7de2ac1160736b161944c9acd79f05
devncore-elena <elena.kim@devncore.org>
2021-09-26 14:01:25 +0900

    Update

--- src/Lol.YamlDatabase/Controller/CodesApi.cs
+++ src/Lol.YamlDatabase/Controller/CodesApi.cs
@@ -1,4 +1,5 @@
-﻿using Lol.YamlDatabase.Entites.Schema;
+﻿using Lol.YamlDatabase.Entites.Core;
+using Lol.YamlDatabase.Entites.Schema;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,31 +11,17 @@ using YamlDotNet.Serialization.NamingConventions;
 
 namespace Lol.YamlDatabase.Controller
 {
-    public class CodesApi
+    public class CodesApi : LolDbContext
     {
         public List<UsualCodes> GetCodes(string codeId)
         {
-            var items = GetCodeItems()
+            var items = Db.CodeItems
                 .Where(x => x.CodeId == codeId)
                 .OrderBy(x => x.Sort)
                 .Select(x => new UsualCodes(x))
                 .ToList();
 
             return items;
-        }
-
-        private List<CodeItems> GetCodeItems()
-        {
-            string file = "/datas/codeitems.yml";
-            string path = Environment.CurrentDirectory + file;
-
-            string readText = File.ReadAllText(path);
-
-            var deserializer = new DeserializerBuilder()
-                .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                .Build();
-
-            return deserializer.Deserialize<List<CodeItems>>(readText);
         }
     }
 }
---
+++ src/Lol.YamlDatabase/Entites/Core/LolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Lol.YamlDatabase.Entites.Statics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.YamlDatabase.Entites.Core
+{
+    public class LolDbContext
+    {
+        protected LolDataPack Db { get; private set; }
+
+        public LolDbContext()
+        {
+            Db = LolDataPack.Instance;
+        }
+    }
+}
