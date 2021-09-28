9d83679306350375115bc28a379dab16bab7bba3
james lee <james.lee@devncore.org>
2021-09-28 23:44:56 +0900

    Add region

--- src/Lol.Main/Local/Work/WindowWork.cs
+++ src/Lol.Main/Local/Work/WindowWork.cs
@@ -5,19 +5,28 @@ namespace Lol.Main.Local.Work
 {
     public class WindowWork
     {
+        #region Constructor
+
         public WindowWork(MainViewModel view)
         {
             _ = view;
         }
+        #endregion
+
+        #region DoClosing
 
         internal void DoClosing(object ui)
         {
             Window.GetWindow((UIElement)ui).Close();
         }
+        #endregion
+
+        #region DoMinizing
 
         internal void DoMinizing(object ui)
         {
             Window.GetWindow((UIElement)ui).WindowState = WindowState.Minimized;
         }
+        #endregion
     }
 }
