03733eab96267e5c7f56af0367326dc0c3ccdebb
james lee <james.lee@devncore.org>
2021-09-30 02:20:07 +0900

    Update

--- src/Lol.Foundation/Mvvm/ObservableData.cs
+++ src/Lol.Foundation/Mvvm/ObservableData.cs
@@ -1,9 +1,4 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows;
-using Lol.Foundation.Riotcore;
-
-namespace Lol.Foundation.Mvvm
+﻿namespace Lol.Foundation.Mvvm
 {
-   
+
 }
