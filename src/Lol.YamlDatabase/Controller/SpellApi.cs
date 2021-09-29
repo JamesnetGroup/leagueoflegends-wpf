0cad91a794f51e5feee914457e17c27d3fa1b7a5
james lee <james.lee@devncore.org>
2021-09-30 03:19:51 +0900

    Add spell data.

---
+++ src/Lol.YamlDatabase/Controller/SpellApi.cs
@@ -0,0 +1,13 @@
+﻿using Lol.YamlDatabase.Entites.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.YamlDatabase.Controller
+{
+    public class SpellApi : LolDbContext
+    {
+    }
+}
