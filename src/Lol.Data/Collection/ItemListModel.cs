50d546e3e5f54b6352b0d66c5eef533f6cc9b727
nuhabo <nuhabo03@gmail.com>
2021-09-29 23:06:59 +0900

    update

--- src/Lol.Data/Collection/ItemListModel.cs
+++ src/Lol.Data/Collection/ItemListModel.cs
@@ -3,52 +3,17 @@ using System.Runtime.CompilerServices;
 
 namespace Lol.Data.Collection
 {
-    public class ItemListModel : INotifyPropertyChanged
+    public class ItemListModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-        private int _Seq;
-        public int Seq
-        {
-            get { return _Seq; }
-            set { _Seq = value; OnPropertyChanged(); }
-        }
+        public int Seq { get; set; }
 
+        public string Name { get; set; }
 
-        private string _Name;
-        public string Name
-        {
-            get { return _Name; }
-            set { _Name = value; OnPropertyChanged(); }
-        }
+        public string Champ { get; set; }
 
+        public string MapType1 { get; set; }
 
-        private string _Champ;
-        public string Champ
-        {
-            get { return _Champ; }
-            set { _Champ = value; OnPropertyChanged(); }
-        }
-
-
-        private string _MapType1;
-        public string MapType1
-        {
-            get { return _MapType1; }
-            set { _MapType1 = value; OnPropertyChanged(); }
-        }
-
-
-        private string _MapType2;
-        public string MapType2
-        {
-            get { return _MapType2; }
-            set { _MapType2 = value; OnPropertyChanged(); }
-        }
-
+        public string MapType2 { get; set; }
 
     }
 }
