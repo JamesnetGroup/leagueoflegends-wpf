496c59a0b266c2a6b7d37f0c35df1df91f496d16
nuhabo <nuhabo03@gmail.com>
2021-10-01 02:21:47 +0900

    update

--- src/Lol.YamlDatabase/Entites/Schema/Items.cs
+++ src/Lol.YamlDatabase/Entites/Schema/Items.cs
@@ -8,9 +8,11 @@
 
         public string Champ { get; set; }
 
-        public string MapType1 { get; set; }
+        public string[] MapType1 { get; set; }
+        public string MapTypeStr1 { get; set; }
 
-        public string MapType2 { get; set; }
+        public string[] MapType2 { get; set; }
+        public string MapTypeStr2 { get; set; }
 
     }
 }
