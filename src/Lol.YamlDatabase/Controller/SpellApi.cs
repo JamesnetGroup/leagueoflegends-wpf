121be4aba501045bff1ddae9e1373af1e8124ddf
james lee <james.lee@devncore.org>
2021-09-30 22:20:20 +0900

    Update

--- src/Lol.YamlDatabase/Controller/SpellApi.cs
+++ src/Lol.YamlDatabase/Controller/SpellApi.cs
@@ -7,10 +7,20 @@ namespace Lol.YamlDatabase.Controller
     {
         public List<Spells> GetSpells()
         {
-            var playTypes = Db.PlayTypes;
-            var spells = Db.Spells;
-            spells.ForEach(x => x.PlayTypeItems = playTypes.Where(y=>x.PlayTypes.Contains(y.Id)).ToList());
-            return spells;
+            var query = from s in Db.Spells
+                        select new Spells
+                        {
+                            Seq = s.Seq,
+                            Name = s.Name,
+                            Content = s.Content,
+                            PlayTypeItems = Db.PlayTypes.Where(y => s.PlayTypes.Contains(y.Id)),
+                            Delay = s.Delay,
+                            Level = s.Level
+                        };
+            return query.ToList();
         }
     }
 }
+
+// TODO: [James] 임시 Foreach Linq문 참고용.
+//spells.ForEach(x => x.PlayTypeItems = playTypes.Where(y => x.PlayTypes.Contains(y.Id)));
