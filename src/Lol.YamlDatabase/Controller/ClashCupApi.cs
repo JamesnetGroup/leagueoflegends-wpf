110364952b46798cbf25be539a0c27fc5d03f5c1
james lee <james.lee@devncore.org>
2021-10-03 16:09:33 +0900

    Update

--- src/Lol.YamlDatabase/Controller/ClashCupApi.cs
+++ src/Lol.YamlDatabase/Controller/ClashCupApi.cs
@@ -7,6 +7,18 @@ namespace Lol.YamlDatabase.Controller
 {
     public class ClashCupApi : LolDbContext
     {
+        public List<ClashTabs> GetClashTabs()
+        {
+            var items = from s in Db.ClashTabs
+                        select new ClashTabs
+                        {
+                            Seq = s.Seq,
+                            Name = s.Name
+                        };
+
+            return items.ToList();
+        }
+
         public List<ClashCups> GetClashCups()
         {
             var items = from s in Db.ClashCups
@@ -18,13 +30,31 @@ namespace Lol.YamlDatabase.Controller
 
             return items.ToList();
         }
+
         public List<ClashTiers> GetClashTiers()
         {
-            var items = from s in Db.ClashCups
+            var items = from s in Db.ClashTiers
                         select new ClashTiers
                         {
                             Seq = s.Seq,
                             Name = s.Name
+                        };
+
+            return items.ToList();
+        }
+
+        public List<ClashTierDetails> GetClashTierDetails(int seq)
+        {
+            var items = from s in Db.ClashTierDetails
+                        where s.ParentSeq == seq
+                        select new ClashTierDetails
+                        {
+                            Seq = s.Seq,
+                            ParentSeq = s.ParentSeq,
+                            Id = s.Id,
+                            Name = s.Name,
+                            SubName = s.SubName,
+                            Time = s.Time
                         };
 
             return items.ToList();
