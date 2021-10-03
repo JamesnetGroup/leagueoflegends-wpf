86e985130783212d86e3a301acc4c605707dbd96
nuhabo <nuhabo03@gmail.com>
2021-10-04 00:07:34 +0900

    update

--- src/Lol.YamlDatabase/Controller/ChampApi.cs
+++ src/Lol.YamlDatabase/Controller/ChampApi.cs
@@ -1,10 +1,21 @@
 ﻿using Lol.YamlDatabase.Entites.Core;
+using Lol.YamlDatabase.Entites.Schema;
 
 namespace Lol.YamlDatabase.Controller
 {
     public class ChampApi : LolDbContext
     {
-
+        public List<Champs> GetChamps()
+        {
+            var query = from c in Db.Champs
+                        select new Champs
+                        {
+                            Seq = c.Seq,
+                            Champ = c.Champ,
+                            Name = c.Name
+                        };
+            return query.ToList();
+        }
 
     }
 }
