684da7c029b7d4e1ef54a6360fcd9d679ab9d5ac
nuhabo <nuhabo03@gmail.com>
2021-10-04 03:46:56 +0900

    update

---
+++ src/Lol.Collection/Local/ViewModel/RuneViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Lol.Foundation.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.Collection.Local.ViewModel
+{
+    public class RuneViewModel : ObservableObject
+    {
+
+    }
+}
---
+++ src/Lol.Collection/UI/Views/RuneView.cs
@@ -0,0 +1,30 @@
+﻿using Lol.LayoutSupport.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lol.Collection.UI.Views
+{
+    public class RuneView : View
+    {
+        #region DefaultStyleKey
+
+
+        static RuneView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RuneView), new FrameworkPropertyMetadata(typeof(RuneView)));
+        }
+        #endregion
+    }
+}
