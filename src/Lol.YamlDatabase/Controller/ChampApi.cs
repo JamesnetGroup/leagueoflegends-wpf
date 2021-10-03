9b7d8c3ef77e539fd972c62452d27b0d149644fd
nuhabo <nuhabo03@gmail.com>
2021-10-03 21:42:07 +0900

    champImg

---
+++ src/Lol.YamlDatabase/Controller/ChampApi.cs
@@ -0,0 +1,15 @@
+﻿using Lol.YamlDatabase.Entites.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.YamlDatabase.Controller
+{
+    public class ChampApi : LolDbContext
+    {
+
+
+    }
+}
