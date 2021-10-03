efa9b7262cbffd4c1796398a724771a4064af700
rlagurdls456@outlook.com <rlagurdls456@outlook.com>
2021-10-03 14:42:42 +0900

    GetHistory Remove

--- src/Lol.ExampleData/History/GetPlayChampions.cs
+++ src/Lol.ExampleData/History/GetPlayChampions.cs
@@ -9,9 +9,9 @@ namespace Lol.ExampleData.History
         {
             List<HistoryModel> source = new()
             {
-                new HistoryModel { Champion = imgResource("Square", "1"), Percent = "19%" },
-                new HistoryModel { Champion = imgResource("Square", "2"), Percent = "14%" },
-                new HistoryModel { Champion = imgResource("Square", "3"), Percent = "15%" }
+                new HistoryModel { Champion = ImgResource("Square", "1"), Percent = "19%" },
+                new HistoryModel { Champion = ImgResource("Square", "2"), Percent = "14%" },
+                new HistoryModel { Champion = ImgResource("Square", "3"), Percent = "15%" }
             };
 
             return source;
