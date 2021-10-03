d352f737ad573324e29c646417baa86c0dc63f00
rlagurdls456@outlook.com <rlagurdls456@outlook.com>
2021-10-03 16:05:04 +0900

    Update

---
+++ src/Lol.YamlDatabase/Controller/StoreApi.cs
@@ -0,0 +1,24 @@
+﻿using Lol.YamlDatabase.Entites.Core;
+using Lol.YamlDatabase.Entites.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.YamlDatabase.Controller
+{
+    public class StoreApi : LolDbContext
+    {
+        public List<StoreMenus> GetCategory()
+        {
+            var query = from s in Db.StoreMenus
+                        select new StoreMenus
+                        {
+                            Seq = s.Seq,
+                            Name = s.Name,
+                        };
+            return query.ToList();
+        }
+    }
+}
---
+++ src/Lol.YamlDatabase/Entites/Schema/StoreMenus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lol.YamlDatabase.Entites.Schema
+{
+    public class StoreMenus
+    {
+        public int Seq { get; set; }
+        public string Name { get; set; }
+        public ICommand MenuSelectCommand { get; set; }
+
+        //public StoreMenus(int seq, string name)
+        //{
+        //    Seq = seq;
+        //    Name = name;
+        //}
+    }
+}
