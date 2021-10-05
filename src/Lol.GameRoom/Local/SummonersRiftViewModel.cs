f95e98acaec4cad545026914b579804086d091ac
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-05 22:31:57 +0900

    Update

--- src/Lol.GameRoom/Local/SummonersRiftViewModel.cs
+++ src/Lol.GameRoom/Local/SummonersRiftViewModel.cs
@@ -1,8 +1,16 @@
 ﻿using Lol.Foundation.Mvvm;
+using System;
 
 namespace Lol.GameRoom.Local
 {
     public class SummonersRiftViewModel : ObservableObject
     {
+        
+
+        public SummonersRiftViewModel()
+        {
+        }
+
+      
     }
 }
