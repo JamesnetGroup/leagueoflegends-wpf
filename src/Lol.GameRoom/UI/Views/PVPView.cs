dbea3b9e15ddc3a527ff7859019d09de88c93559
devncore-elena <elena.kim@devncore.org>
2021-10-05 20:50:56 +0900

    Update GameRoom

--- src/Lol.GameRoom/UI/Views/PVPView.cs
+++ src/Lol.GameRoom/UI/Views/PVPView.cs
@@ -5,9 +5,12 @@ namespace Lol.GameRoom.UI.Views
 {
     public class PVPView : View
     {
+        #region DefaultStyleKey
+
         static PVPView()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PVPView), new FrameworkPropertyMetadata(typeof(PVPView)));
         }
+        #endregion
     }
 }
