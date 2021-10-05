707aabbdb496a0b47dab076dad6aa376543f88a8
devncore-elena <elena.kim@devncore.org>
2021-10-05 23:54:57 +0900

    Update HistoryView

---
+++ src/Lol.Profile/UI/Units/GameHistory.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Profile.UI.Units
+{
+    public class GameHistory : ListBox
+    {
+        #region DefaultStyleKey
+
+        static GameHistory()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GameHistory), new FrameworkPropertyMetadata(typeof(GameHistory)));
+        }
+        #endregion
+    }
+}
---
+++ src/Lol.Profile/UI/Units/PlayedChampList.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Profile.UI.Units
+{
+    public class PlayedChampList : ListBox
+    {
+        #region DefaultStyleKey
+
+        static PlayedChampList()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlayedChampList), new FrameworkPropertyMetadata(typeof(PlayedChampList)));
+        }
+        #endregion
+    }
+}
