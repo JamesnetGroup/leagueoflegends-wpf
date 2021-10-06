c10e447ff8429e90466e1cd44f3a2d3df5f58732
devncore-elena <elena.kim@devncore.org>
2021-10-06 19:23:03 +0900

    Update GameRoom PVPView

---
+++ src/Lol.GameRoom/UI/Units/MapList.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class MapList : ListBox
+    {
+        #region DefaultStyleKey
+
+        static MapList()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MapList), new FrameworkPropertyMetadata(typeof(MapList)));
+        }
+        #endregion
+    }
+}
