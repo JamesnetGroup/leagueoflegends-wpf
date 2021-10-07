88d4c8f608a68c2920d5d7e0b04c4ec59b4cb763
devncore-elena <elena.kim@devncore.org>
2021-10-07 20:34:04 +0900

    Add MapSymbol

---
+++ src/Lol.GameRoom/UI/Units/MapSymbol.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class MapSymbol : Control
+    {
+        #region DefaultStyleKey
+
+        static MapSymbol()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MapSymbol), new FrameworkPropertyMetadata(typeof(MapSymbol)));
+        }
+        #endregion
+    }
+}
