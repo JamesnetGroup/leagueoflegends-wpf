c1ef5bbe0988d65e5480524706145868bfadfd05
devncore-elena <elena.kim@devncore.org>
2021-10-07 20:41:03 +0900

    Add PVPView MapTitle

---
+++ src/Lol.GameRoom/UI/Units/MapTitle.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class MapTitle : Control
+    {
+        #region DefaultStyleKey
+
+        static MapTitle()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MapTitle), new FrameworkPropertyMetadata(typeof(MapTitle)));
+        }
+        #endregion
+    }
+}
