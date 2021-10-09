a60e95134d1adf3203ce6f47c0981673b0f24329
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-09 20:09:00 +0900

    Update

---
+++ src/Lol.GameRoom/UI/Units/StateRadioButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class StateRadioButton : RadioButton
+    {
+        #region DefaultStyleKey
+
+        static StateRadioButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StateRadioButton), new FrameworkPropertyMetadata(typeof(StateRadioButton)));
+        }
+        #endregion
+    }
+}
