ace530bafb111b3b60a329e21b70829845bdd9d7
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-10 16:44:24 +0900

    Update

--- src/Lol.GameRoom/Local/ViewModels/SummonersRiftViewModel.cs
+++ src/Lol.GameRoom/Local/ViewModels/SummonersRiftViewModel.cs
@@ -1,14 +1,29 @@
 ﻿using Lol.Foundation.Mvvm;
+using Lol.YamlDatabase.Collection;
+using Lol.YamlDatabase.Controller;
+using Lol.YamlDatabase.Entites.Schema;
+using System.Collections.Generic;
 
 namespace Lol.GameRoom.Local.ViewModels
 {
     public class SummonersRiftViewModel : ObservableObject
     {
+        private FriendsCollection _friends;
+
+        #region Friends
+
+        public FriendsCollection Friends
+        {
+            get => _friends;
+            set { _friends = value; OnPropertyChanged(); }
+        }
+        #endregion
+
         #region Constructor
 
-        public SummonersRiftViewModel()
+        public SummonersRiftViewModel(FriendsCollection friends)
         {
-
+            Friends = friends;
         }
         #endregion
     }
