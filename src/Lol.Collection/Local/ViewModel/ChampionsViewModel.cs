9c43e2923d255b950783034821181f637deab525
nuhabo <nuhabo03@gmail.com>
2021-10-13 00:28:23 +0900

    champ combobox

--- src/Lol.Collection/Local/ViewModel/ChampionsViewModel.cs
+++ src/Lol.Collection/Local/ViewModel/ChampionsViewModel.cs
@@ -1,14 +1,141 @@
-﻿using Lol.Foundation.Mvvm;
+﻿using Lol.Data.Store;
+using Lol.Foundation.Mvvm;
+using Lol.YamlDatabase.Controller;
+using Lol.YamlDatabase.Entites.Schema;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lol.Collection.Local.ViewModel
 {
     public class ChampionsViewModel : ObservableObject
     {
+        #region Variables
+
+        private List<StoreChampTypes> _champMenus;
+        private List<StoreChampSortings> _sorting;
+        private List<FilterModel> _filters;
+
+        private StoreChampTypes _currentChampMenu;
+        private StoreChampSortings _currentSorting;
+        private string _searchText;
+        #endregion
+
+        #region ChampMenus
+
+        public List<StoreChampTypes> ChampMenus
+        {
+            get { return _champMenus; }
+            set { _champMenus = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region CurrentChampMenu
+
+        public StoreChampTypes CurrentChampMenu
+        {
+            get { return _currentChampMenu; }
+            set { _currentChampMenu = value; OnPropertyChanged(); MenuChanged(value); }
+        }
+        #endregion
+
+        #region Sorting
+
+        public List<StoreChampSortings> Sorting
+        {
+            get { return _sorting; }
+            set { _sorting = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region CurrentSorting
+
+        public StoreChampSortings CurrentSorting
+        {
+            get { return _currentSorting; }
+            set { _currentSorting = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region SearchText
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set { _searchText = value; OnPropertyChanged(); SearchTextChanged(value); }
+        }
+        #endregion
+
+        #region Filters
+
+        public List<FilterModel> Filters
+        {
+            get { return _filters; }
+            set { _filters = value; OnPropertyChanged(); }
+        }
+        #endregion
+
         #region Constructor
 
         public ChampionsViewModel()
         {
+            ChampMenus = new StoreApi().GetCategory();
+            CurrentChampMenu = ChampMenus.First();
+        }
+        #endregion
 
+        #region MenuChanged
+
+        private void MenuChanged(StoreChampTypes value)
+        {
+            string id = value.Name == "BUNDLES" ? value.Name : "STANDARD";
+
+            Filters = GetFilters(value.Name);
+            Sorting = new StoreApi().GetSorting(id);
+            CurrentSorting = Sorting.First();
+        }
+        #endregion
+
+        #region SearchTextChanged
+
+        private void SearchTextChanged(string _)
+        {
+            if (CurrentChampMenu.Name == "CHAMPIONS")
+            {
+                //
+            }
+        }
+        #endregion
+
+        #region Temp data
+
+        public static List<FilterModel> filters = new()
+        {
+            //new FilterModel(ChampType.Assassin, "Assassin", true, true, false),
+            //new FilterModel(ChampType.Fighter, "Fighter", true, true, false),
+            //new FilterModel(ChampType.Mage, "Mage", true, true, false),
+            //new FilterModel(ChampType.Marksman, "Marksman", true, true, false),
+            //new FilterModel(ChampType.Support, "Support", true, true, false),
+            //new FilterModel(ChampType.Tanker, "Tank", true, true, false),
+            //new FilterModel(PackageType.StarterSet, "Starter Series", false, true, false),
+            //new FilterModel(PackageType.SeriesA, "Series 1", false, true, false),
+            //new FilterModel(PackageType.OnSale, "On Sale", true, true, false),
+            //new FilterModel(PackageType.Limited, "Limited Availability", false, false, true)
+        };
+
+        public static List<FilterModel> GetFilters(string name)
+        {
+            List<FilterModel> source = new();
+
+            switch (name)
+            {
+                case "CHAMPIONS": source = filters.Where(x => x.IsChampionVisible).ToList(); break;
+                case "ETERNALS": source = filters.Where(x => x.IsEternalVisible).ToList(); break;
+                case "BUNDLES": source = filters.Where(x => x.IsBundleVisible).ToList(); break;
+                default:
+                    break;
+            }
+
+            return source;
         }
         #endregion
     }
---
+++ src/Lol.Collection/UI/Units/SortComboBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lol.Collection.UI.Units
+{
+    public class SortComboBox : ComboBox
+    {
+        #region DefalutStyleKey
+
+        static SortComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SortComboBox), new FrameworkPropertyMetadata(typeof(SortComboBox)));
+        }
+        #endregion
+
+        #region DependencyProperty
+
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(object), typeof(SortComboBox), new PropertyMetadata(null));
+        #endregion
+
+        #region Content
+
+        public object Content
+        {
+            get { return (object)this.GetValue(ContentProperty); }
+            set { this.SetValue(ContentProperty, value); }
+        }
+        #endregion
+
+        #region GetContainerForItemOverride
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new SortComboBoxItem();
+        }
+        #endregion
+    }
+}
