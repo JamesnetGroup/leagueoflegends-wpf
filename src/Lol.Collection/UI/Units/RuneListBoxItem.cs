f2f743ed34ba519986a54505d1dd91fe0638f27f
devncore-elena <elena.kim@devncore.org>
2021-10-13 23:03:59 +0900

    Clean Collection Units

--- src/Lol.Collection/UI/Units/RuneListBoxItem.cs
+++ src/Lol.Collection/UI/Units/RuneListBoxItem.cs
@@ -5,9 +5,12 @@ namespace Lol.Collection.UI.Units
 {
     public class RuneListBoxItem : ListBoxItem
     {
+        #region DefaultStyleKey
+
         static RuneListBoxItem()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(RuneListBoxItem), new FrameworkPropertyMetadata(typeof(RuneListBoxItem)));
         }
+        #endregion
     }
 }
