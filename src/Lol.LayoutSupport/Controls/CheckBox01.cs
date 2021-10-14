3ec13c7a768ef7899528ea7ccb80abc5c9533fc4
devncore-elena <elena.kim@devncore.org>
2021-10-14 21:00:30 +0900

    Update ItemView ItemSet

--- src/Lol.LayoutSupport/Controls/CheckBox01.cs
+++ src/Lol.LayoutSupport/Controls/CheckBox01.cs
@@ -12,5 +12,20 @@ namespace Lol.LayoutSupport.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBox01), new FrameworkPropertyMetadata(typeof(CheckBox01)));
         }
         #endregion
+
+        #region DependencyProperty
+
+        public static readonly DependencyProperty TooltipPaddingProperty = DependencyProperty.Register(
+            "TooltipPadding", typeof(Thickness), typeof(CheckBox01), new PropertyMetadata(default(Thickness)));
+        #endregion
+
+        #region TooltipPadding
+
+        public Thickness TooltipPadding
+        {
+            get { return (Thickness)GetValue(TooltipPaddingProperty); }
+            set { SetValue(TooltipPaddingProperty, value); }
+        }
+        #endregion
     }
 }
