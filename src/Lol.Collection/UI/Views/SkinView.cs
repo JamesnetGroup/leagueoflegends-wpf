6f296ad1fd3852faa0e960c10ef8efb4c6953cdf
devncore-elena <elena.kim@devncore.org>
2021-10-17 22:32:28 +0900

    Add SkinView and Resources

---
+++ src/Lol.Collection/UI/Views/SkinView.cs
@@ -0,0 +1,16 @@
+﻿using Lol.LayoutSupport.Controls;
+using System.Windows;
+
+namespace Lol.Collection.UI.Views
+{
+    public class SkinView : View
+    {
+        #region DefaultStyleKey
+
+        static SkinView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SkinView), new FrameworkPropertyMetadata(typeof(SkinView)));
+        }
+        #endregion
+    }
+}
