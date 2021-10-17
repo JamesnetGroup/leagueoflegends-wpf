386c53018d474de5132d7603db5218674f487dce
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-17 22:00:18 +0900

    Add Loot CommonListBox

---
+++ src/Lol.Loot/UI/Units/CommonListBoxItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Loot.UI.Units
+{
+    public class CommonListBoxItem : ListBoxItem
+    {
+        #region DefaultStyleKey
+
+        static CommonListBoxItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommonListBoxItem), new FrameworkPropertyMetadata(typeof(CommonListBoxItem)));
+        }
+        #endregion
+    }
+}
