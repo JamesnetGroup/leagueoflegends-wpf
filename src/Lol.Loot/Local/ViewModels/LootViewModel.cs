6db902408c2ee6448afd393dad9de8aea49b65cd
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-17 15:22:36 +0900

    Update Loot LeftMenu

--- src/Lol.Loot/Local/ViewModels/LootViewModel.cs
+++ src/Lol.Loot/Local/ViewModels/LootViewModel.cs
@@ -1,14 +1,31 @@
 ﻿using Lol.Foundation.Mvvm;
+using Lol.YamlDatabase.Controller;
+using Lol.YamlDatabase.Entites.Schema;
+using System.Collections.Generic;
 
 namespace Lol.Loot.Local.ViewModels
 {
     public class LootViewModel : ObservableObject
     {
+        #region Variables
+
+        private List<Loots> _menus;
+        #endregion
+
+        #region Menus
+
+        public List<Loots> Menus
+        {
+            get { return _menus; }
+            set { _menus = value; OnPropertyChanged(); }
+        }
+        #endregion
+
         #region Constructor
 
         public LootViewModel()
         {
-
+            Menus = new LootApi().GetLoots();
         }
         #endregion
     }
---
+++ src/Lol.YamlDatabase/Controller/LootApi.cs
@@ -0,0 +1,23 @@
+﻿using Lol.YamlDatabase.Entites.Core;
+using Lol.YamlDatabase.Entites.Schema;
+
+namespace Lol.YamlDatabase.Controller
+{
+    public class LootApi : LolDbContext
+    {
+        #region GetLoots
+
+        public List<Loots> GetLoots()
+        {
+            var query = from i in Db.Loots
+                        select new Loots
+                        {
+                            Seq = i.Seq,
+                            Name = i.Name,
+                            Type = i.Type,
+                        };
+            return query.ToList();
+        }
+        #endregion
+    }
+}
