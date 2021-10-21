c3ce092b3f9b81459e881ce01a9dd5b4ef2ae671
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-21 23:10:36 +0900

    Update

---
+++ src/Lol.Loot/UI/Units/CommonListItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Loot.UI.Units
+{
+    public class CommonListItem : Button
+    {
+        #region DefaultStyleKey
+
+        static CommonListItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommonListItem), new FrameworkPropertyMetadata(typeof(CommonListItem)));
+        }
+        #endregion
+    }
+}
