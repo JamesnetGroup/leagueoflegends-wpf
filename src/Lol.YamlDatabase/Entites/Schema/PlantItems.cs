8a33e970f7631a7084c407a13e275fb66835cf54
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-24 15:12:20 +0900

    Update yaml

--- src/Lol.YamlDatabase/Entites/Schema/PlantItems.cs
+++ src/Lol.YamlDatabase/Entites/Schema/PlantItems.cs
@@ -8,6 +8,7 @@ namespace Lol.YamlDatabase.Entites.Schema
 {
     public class PlantHeaders
     {
+        public int Seq { get; set; }
         public bool IsExpanded { get; set; } = true;
         public List<PlantItems> Children { get; set; }
         public string Header { get; set; }
@@ -16,6 +17,7 @@ namespace Lol.YamlDatabase.Entites.Schema
     public class PlantItems
     {
         public int Seq { get; set; }
+        public int HeaderSeq { get; set; }
         public string Content { get; set; }
     }
 }
