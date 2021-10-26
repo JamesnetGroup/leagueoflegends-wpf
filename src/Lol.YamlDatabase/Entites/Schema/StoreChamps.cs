ecef1f51dcf7ac0cca5e1acd17341c1afbc10cae
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-26 21:51:03 +0900

    Add Store Yaml

---
+++ src/Lol.YamlDatabase/Entites/Schema/StoreChamps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.YamlDatabase.Entites.Schema
+{
+    public class StoreChamps
+    {
+        public int Seq { get; set; }
+        public string Name { get; set; }
+        public string ImgSource { get; set; }
+        public int RP { get; set; }
+        public int BE { get; set; }
+    }
+}
