235c7c5eb013f31952242201cb1f8f8a43fbf7e5
nuhabo <nuhabo03@gmail.com>
2021-10-29 23:32:43 +0900

    Update highlight

---
+++ src/Lol.Profile/UI/Views/HighlightView.cs
@@ -0,0 +1,30 @@
+﻿using Lol.LayoutSupport.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lol.Profile.UI.Views
+{
+    public class HighlightView : View
+    {
+        #region DefalutStyleKey
+
+
+        static HighlightView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HighlightView), new FrameworkPropertyMetadata(typeof(HighlightView)));
+        }
+        #endregion
+    }
+}
