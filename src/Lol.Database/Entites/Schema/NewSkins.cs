bbfb514bde776b7389925fef09d6b1d49af85caa
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-30 17:46:07 +0900

    Update Store

---
+++ src/Lol.Database/Entites/Schema/NewSkins.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.Database.Entites.Schema
+{
+    public class NewSkins
+    {
+        public int Seq { get; set; }
+        public string SkinValue { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        //public string ImgSource { get; set; }
+    }
+}
--- src/Lol.Store/Local/ViewModels/HomeViewModel.cs
+++ src/Lol.Store/Local/ViewModels/HomeViewModel.cs
@@ -1,14 +1,31 @@
-﻿using Lol.Foundation.Mvvm;
+﻿using Lol.Database.Controller;
+using Lol.Database.Entites.Schema;
+using Lol.Foundation.Mvvm;
+using System.Collections.Generic;
 
 namespace Lol.Store.Local.ViewModels
 {
     public class HomeViewModel : ObservableObject
     {
+        #region Variables
+
+        private List<NewSkins> _newSkins;
+        #endregion
+
+        #region NewSkins
+
+        public List<NewSkins> NewSkins
+        {
+            get { return _newSkins; }
+            set { _newSkins = value; OnPropertyChanged(); }
+        }
+        #endregion
+
         #region Constructor
 
         public HomeViewModel()
         {
-
+            NewSkins = new StoreApi().GetNewSkins();
         }
         #endregion
     }
