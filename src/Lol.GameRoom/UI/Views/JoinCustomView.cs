37cf66dc535cb306424c7a9d84238f1ab713cae7
nuhabo <nuhabo03@gmail.com>
2021-10-30 10:22:47 +0900

    Update JoinCustom

---
+++ src/Lol.GameRoom/UI/Views/JoinCustomView.cs
@@ -0,0 +1,30 @@
+﻿using Lol.LayoutSupport.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lol.GameRoom.UI.Views
+{
+    public class JoinCustomView : View
+    {
+        #region DefaultStyleKey
+
+
+        static JoinCustomView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(JoinCustomView), new FrameworkPropertyMetadata(typeof(JoinCustomView)));
+        }
+        #endregion
+    }
+}
