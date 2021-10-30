65dcf5c6cf9584ad29336c097b256e0720cbf201
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-30 21:27:36 +0900

    Add Store Thread

--- src/Lol.Store/Local/ViewModels/HomeViewModel.cs
+++ src/Lol.Store/Local/ViewModels/HomeViewModel.cs
@@ -2,6 +2,7 @@
 using Lol.Database.Entites.Schema;
 using Lol.Foundation.Mvvm;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Lol.Store.Local.ViewModels
 {
@@ -37,6 +38,24 @@ namespace Lol.Store.Local.ViewModels
         {
             NewSkins = new StoreApi().GetNewSkins();
             CurrentSkin = NewSkins[0];
+
+            Thread thread = new Thread(() =>
+            {
+                while(true)
+                {
+                    Thread.Sleep(5000);
+
+                    int index = NewSkins.IndexOf(CurrentSkin);
+
+                    if (index == NewSkins.Count - 1)
+                        index = -1;
+
+                    CurrentSkin = NewSkins[index + 1];
+                }
+            });
+
+            thread.IsBackground = true;
+            thread.Start();
         }
         #endregion
     }
