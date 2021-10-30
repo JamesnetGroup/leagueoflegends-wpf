ea7e96ed10f7b9213fae5ae4e378c219687de09d
nuhabo <nuhabo03@gmail.com>
2021-10-30 10:22:47 +0900

    Update JoinCustom

---
+++ src/Lol.GameRoom/Local/ViewModels/JoinCustomViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Lol.Foundation.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.GameRoom.Local.ViewModels
+{
+    public class JoinCustomViewModel : ObservableObject
+    {
+
+
+    }
+}
