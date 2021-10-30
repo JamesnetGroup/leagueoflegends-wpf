c9ca800f8a5ee5255a14c92a2ea12d32717612e5
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-30 16:33:21 +0900

    Update Store

--- src/Lol.Store/Local/ViewModels/HomeViewModel.cs
+++ src/Lol.Store/Local/ViewModels/HomeViewModel.cs
@@ -1,13 +1,15 @@
 ﻿using Lol.Foundation.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lol.Store.Local.ViewModels
 {
     public class HomeViewModel : ObservableObject
     {
+        #region Constructor
+
+        public HomeViewModel()
+        {
+
+        }
+        #endregion
     }
 }
