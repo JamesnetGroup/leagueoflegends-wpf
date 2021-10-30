8d310592feab9d571098d012619ceaf31949c39d
nuhabo <nuhabo03@gmail.com>
2021-10-31 02:13:17 +0900

    Update CustomGame

---
+++ src/Lol.GameRoom/UI/Units/ConfirmButton2.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class ConfirmButton2 : Button
+    {
+        #region DefaultStyleKey
+
+        static ConfirmButton2()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ConfirmButton2), new FrameworkPropertyMetadata(typeof(ConfirmButton2)));
+        }
+        #endregion
+    }
+}
