dd8ecb790632ce132669945eedf1bccf5d6abe38
rlagurdls456 <rlagurdls456@outlook.com>
2021-10-31 18:52:47 +0900

    Update Store

--- src/Lol.Database/Entites/Schema/Popularities.cs
+++ src/Lol.Database/Entites/Schema/Popularities.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lol.Database.Entites.Schema
+﻿namespace Lol.Database.Entites.Schema
 {
     public class Popularities
     {
         public int Seq { get; set; }
         public string Header { get; set; }
-        //public List<PopularityDetails> Details { get; set; }
+        public List<PopularityDetails> Details { get; set; }
     }
 }
---
+++ src/Lol.Database/Entites/Schema/PopularityDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace Lol.Database.Entites.Schema
+{
+    public class PopularityDetails
+    {
+        public int Seq { get; set; }
+        public int ParentSeq { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string ImgSource { get; set; }
+    }
+}
