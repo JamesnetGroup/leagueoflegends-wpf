f6032b5e9f8ca519e850def897c4e44bed85d4d8
nuhabo <nuhabo03@gmail.com>
2021-10-31 09:14:52 +0900

    CreateCustom

---
+++ src/Lol.GameRoom/UI/Units/MapList2.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class MapList2 : ListBox
+    {
+        #region DefaultStyleKey
+
+        static MapList2()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MapList2), new FrameworkPropertyMetadata(typeof(MapList2)));
+        }
+        #endregion
+    }
+}
