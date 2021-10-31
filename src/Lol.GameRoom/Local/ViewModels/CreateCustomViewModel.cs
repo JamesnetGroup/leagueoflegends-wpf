3bc323342e84f2a0663a990db9c1da1b9bbc5b47
nuhabo <nuhabo03@gmail.com>
2021-10-31 09:49:29 +0900

    Update CustomGame

--- src/Lol.GameRoom/Local/ViewModels/CreateCustomViewModel.cs
+++ src/Lol.GameRoom/Local/ViewModels/CreateCustomViewModel.cs
@@ -1,9 +1,64 @@
-﻿using Lol.Foundation.Mvvm;
+﻿using Lol.Database.Controller;
+using Lol.Database.Entites.Schema;
+using Lol.Foundation.Mvvm;
+using System;
+using System.Collections.Generic;
 
 namespace Lol.GameRoom.Local.ViewModels
 {
     public class CreateCustomViewModel : ObservableObject
     {
+        #region Variables
+
+        private Action<object> _customConfirm;
+        private GameRooms _currentCusTom;
+
+        public List<GameRooms> Customs { get; set; }
+
+        #endregion
+
+        #region ICommand
+        public RelayCommand<object> ConfirmCommand { get; set; }
+        #endregion
+
+        #region CurrentCustom
+
+        public GameRooms CurrentCustom
+        {
+            get { return _currentCusTom; }
+            set { _currentCusTom = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region Constructor
+
+        public CreateCustomViewModel(Action<object> customConfirm)
+        {
+            _customConfirm = customConfirm;
+            Customs = new GameRoomApi().GetGameRooms();
+
+            CurrentCustom = Customs[0];
+            ConfirmCommand = new RelayCommand<object>(RoomCreateCommand, CanRoomCreateCommand);
+        }
+        #endregion
+
+
+        #region RoomCreateCommand
+
+        private void RoomCreateCommand(object obj)
+        {
+            _customConfirm.Invoke(obj);
+        }
+        #endregion
+
+        #region CanRoomCreateCommand
+
+        private bool CanRoomCreateCommand(object obj)
+        {
+            return true;
+        }
+        #endregion
+
 
     }
 }
