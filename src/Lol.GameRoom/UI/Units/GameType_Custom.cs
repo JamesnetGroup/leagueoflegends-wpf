c99031987455c52780dfa610495f13a06f45b4eb
nuhabo <nuhabo03@gmail.com>
2021-10-31 10:26:38 +0900

    Update Custom_Create

---
+++ src/Lol.GameRoom/UI/Units/GameType_Custom.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class GameType_Custom : ListBox
+    {
+        #region DefaultStyleKey
+
+        static GameType_Custom()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GameType_Custom), new FrameworkPropertyMetadata(typeof(GameType_Custom)));
+        }
+        #endregion
+    }
+}
