4faa2105aab8f5bdcbf82c0b356847c50aef2509
devncore-elena <elena.kim@devncore.org>
2021-11-02 21:57:39 +0900

    Update ChampionsView TreeView

---
+++ src/Lol.Collection/UI/Units/Champs/ChampionsList.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Collection.UI.Units
+{
+    public class ChampionsList : TreeView
+    {
+        #region DefaultStyleKey
+
+        static ChampionsList()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ChampionsList), new FrameworkPropertyMetadata(typeof(ChampionsList)));
+        }
+        #endregion
+    }
+}
