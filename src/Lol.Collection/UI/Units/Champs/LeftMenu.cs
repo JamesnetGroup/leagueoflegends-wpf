873949b5b422bcd8493234af06f5a69a2eea9193
devncore-elena <elena.kim@devncore.org>
2021-11-02 21:57:39 +0900

    Update ChampionsView TreeView

---
+++ src/Lol.Collection/UI/Units/Champs/LeftMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Collection.UI.Units
+{
+    public class LeftMenu : Control
+    {
+        #region DefaultStyleKey
+
+        static LeftMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LeftMenu), new FrameworkPropertyMetadata(typeof(LeftMenu)));
+        }
+        #endregion
+    }
+}
--- src/Lol.Database/Entites/Schema/Champions.cs
+++ src/Lol.Database/Entites/Schema/Champions.cs
@@ -8,7 +8,7 @@
         public string Name { get; set; }
         public string Img { get; set; }
         public string Insignia { get; set; }
-        public string 영원석 { get; set; }
+        public int Eternals{ get; set; }
         public Type Type => GetType();
     }
 }
