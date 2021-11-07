c683761536d2c01c5a1fa77e2245961d35ab4465
rlagurdls456 <rlagurdls456@outlook.com>
2021-11-07 18:44:24 +0900

    Update Store

--- src/Lol.Database/Entites/Schema/PopularityDetails.cs
+++ src/Lol.Database/Entites/Schema/PopularityDetails.cs
@@ -7,5 +7,6 @@
         public string Name { get; set; }
         public int Value { get; set; }
         public string ImgSource { get; set; }
+        public double Discount { get; set; }
     }
 }
