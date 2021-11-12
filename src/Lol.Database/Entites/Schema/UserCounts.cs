bf423a8788c2ce0ce89f69dbf7325155011b3110
nuhabo <nuhabo03@gmail.com>
2021-11-12 21:36:45 +0900

    Update UserCount CbBox

---
+++ src/Lol.Database/Entites/Schema/UserCounts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.Database.Entites.Schema
+{
+    public class UserCounts
+    {
+        public int Seq { get; set; }    
+        public int UserCount { get; set; }
+    }
+}
