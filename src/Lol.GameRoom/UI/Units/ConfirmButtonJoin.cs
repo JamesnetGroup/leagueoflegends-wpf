1d44b19fbf3ea10090fb95ee5d3ed1c4941268a6
unknown <lucas.park@devncore.org>
2021-11-16 21:42:56 +0900

    Update ClassName 수정

---
+++ src/Lol.GameRoom/UI/Units/ConfirmButtonJoin.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.GameRoom.UI.Units
+{
+    public class ConfirmButtonJoin : Button
+    {
+        #region DefaultStyleKey
+
+        static ConfirmButtonJoin()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ConfirmButtonJoin), new FrameworkPropertyMetadata(typeof(ConfirmButtonJoin)));
+        }
+        #endregion
+    }
+}
