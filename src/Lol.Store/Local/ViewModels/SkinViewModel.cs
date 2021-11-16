4054173ab352c2271652494b6e9cc0af2b18761e
kevin.kim <rlagurdls456@outlook.com>
2021-11-16 22:09:58 +0900

    Update Store

--- src/Lol.Store/Local/ViewModels/SkinViewModel.cs
+++ src/Lol.Store/Local/ViewModels/SkinViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Lol.Database.Controller;
+﻿using Lol.Data.Enums;
+using Lol.Data.Store;
+using Lol.Database.Controller;
 using Lol.Database.Entites.Schema;
 using Lol.Foundation.Mvvm;
 using System.Collections.Generic;
@@ -12,6 +14,11 @@ namespace Lol.Store.Local.ViewModels
 
         private List<StoreSkinTypes> _skinMenus;
         private StoreSkinTypes _currentSkinMenu;
+
+        private List<StoreChampSortings> _sorting;
+        private StoreChampSortings _currentSorting;
+
+        private List<FilterModel> _filters;
         #endregion
 
         #region SkinMenus
@@ -28,7 +35,34 @@ namespace Lol.Store.Local.ViewModels
         public StoreSkinTypes CurrentSkinMenu
         {
             get { return _currentSkinMenu; }
-            set { _currentSkinMenu = value; OnPropertyChanged(); }
+            set { _currentSkinMenu = value; OnPropertyChanged(); MenuChanged(value); }
+        }
+        #endregion
+
+        #region Sorting
+
+        public List<StoreChampSortings> Sorting
+        {
+            get { return _sorting; }
+            set { _sorting = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region CurrentSorting
+
+        public StoreChampSortings CurrentSorting
+        {
+            get { return _currentSorting; }
+            set { _currentSorting = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region Filters
+
+        public List<FilterModel> Filters
+        {
+            get { return _filters; }
+            set { _filters = value; OnPropertyChanged(); }
         }
         #endregion
 
@@ -38,7 +72,53 @@ namespace Lol.Store.Local.ViewModels
         {
             SkinMenus = new StoreApi().GetSkinCategory();
             CurrentSkinMenu = SkinMenus.First();
+            
+        }
+        #endregion
+
+        #region MenuChanged
+
+        private void MenuChanged(StoreSkinTypes value)
+        {
+            string id = value.Name == "BUNDLES" ? value.Name : "STANDARD";
+
+            Filters = GetFilters(value.Name);
+
+            Sorting = new StoreApi().GetSorting(id);
+            CurrentSorting = Sorting.First();
+        }
+        #endregion
+
+        #region Temp data
+
+        public static List<FilterModel> filters = new()
+        {
+            new FilterModel(PackageType.Limited, "미보유 챔피언 숨기기", true, true, false),
+            new FilterModel(PackageType.Limited, "한정 판매", true, false, true),
+            new FilterModel(PackageType.Limited, "전설급 상품", true, false, false),
+            new FilterModel(PackageType.Limited, "초월급 상품", true, false, false),
+            new FilterModel(PackageType.Limited, "할인중", true, false, false),
+            new FilterModel(PackageType.Limited, "보유 중인 스킨", false, true, false),
+            new FilterModel(PackageType.Limited, "파랑 정수로 구매 가능", false, true, false),
+        };
+
+        public static List<FilterModel> GetFilters(string name)
+        {
+            List<FilterModel> source = new();
+
+            switch (name)
+            {
+                case "스킨": source = filters.Where(x => x.IsChampionVisible).ToList(); break;
+                case "크로마": source = filters.Where(x => x.IsEternalVisible).ToList(); break;
+                case "세트": source = filters.Where(x => x.IsBundleVisible).ToList(); break;
+                default:
+                    break;
+            }
+
+            return source;
         }
         #endregion
     }
+
+
 }
