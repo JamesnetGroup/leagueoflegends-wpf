36536c163db5d1e7c3953acfc078beb81646df66
kevin.kim <rlagurdls456@outlook.com>
2021-11-17 21:42:57 +0900

    Update Store ChampList > StoreList로 변경

---
+++ src/Lol.Store/UI/Commons/StoreList.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Store.UI.Units
+{
+    public class StoreList : ListBox
+    {
+        #region DefaultStyleKey
+
+        static StoreList()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StoreList), new FrameworkPropertyMetadata(typeof(StoreList)));
+        }
+        #endregion
+    }
+}
