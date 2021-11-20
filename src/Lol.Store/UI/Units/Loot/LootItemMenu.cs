cc43bb262c40f6cb7e2f9515796de5a9b0e7211f
kevin.kim <rlagurdls456@outlook.com>
2021-11-20 18:15:46 +0900

    Create Store LootView

---
+++ src/Lol.Store/UI/Units/Loot/LootItemMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Store.UI.Units
+{
+    public class LootItemMenu : Control
+    {
+        #region DefaultStyleKey
+
+        static LootItemMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LootItemMenu), new FrameworkPropertyMetadata(typeof(LootItemMenu)));
+        }
+        #endregion
+    }
+}
