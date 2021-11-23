581065ba77f3b7725160fef7948b93e2f03cd52f
nuhabo <nuhabo03@gmail.com>
2021-11-24 00:51:48 +0900

    Update Rune SearchText

--- src/Lol.Collection/Local/ViewModel/ItemViewModel.cs
+++ src/Lol.Collection/Local/ViewModel/ItemViewModel.cs
@@ -40,7 +40,6 @@ namespace Lol.Collection.Local.ViewModel
             get { return _searchText; }
             set { _searchText = value; OnPropertyChanged(); ItemTextChanged(value); }
         }
-
         
         #endregion
 
@@ -56,12 +55,6 @@ namespace Lol.Collection.Local.ViewModel
         #region ItemTextChanged
         private void ItemTextChanged(string value)
         {
-            if (string.IsNullOrEmpty(value))
-            {
-                ItemList = new ItemApi().GetItems();
-                return;
-            }
-
             ItemList = new ItemApi().GetItems();
             var list = ItemList.Where(x => x.Name.Contains(value));
             ItemList = list.ToList();
