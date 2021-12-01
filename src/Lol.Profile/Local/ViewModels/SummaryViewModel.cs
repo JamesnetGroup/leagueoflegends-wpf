65aea7a2641cab50caafd6fcec37b1c918d2e85f
kevin.kim <rlagurdls456@outlook.com>
2021-12-01 23:39:03 +0900

    Add Profile SummaryView

---
+++ src/Lol.Profile/Local/ViewModels/SummaryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Lol.Foundation.Mvvm;
+using Lol.Database.Controller;
+using Lol.Database.Entites.Schema;
+
+namespace Lol.Profile.Local.ViewModels
+{
+    public class SummaryViewModel : ObservableObject
+    {
+        #region Constructor
+
+        public SummaryViewModel()
+        {
+           
+        }
+        #endregion
+    }
+}
