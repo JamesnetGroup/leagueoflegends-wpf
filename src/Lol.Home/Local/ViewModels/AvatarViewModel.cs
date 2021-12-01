f72c0f20bdc2524bccd6ba9e614da300d223987e
nuhabo <nuhabo03@gmail.com>
2021-12-02 00:19:55 +0900

    Update Avatar

---
+++ src/Lol.Home/Local/ViewModels/AvatarViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Lol.Foundation.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lol.Home.Local.ViewModels
+{
+    public class AvatarViewModel : ObservableObject
+    {
+        #region Constructor
+
+        public AvatarViewModel()
+        {
+
+        }
+        #endregion
+
+
+    }
+}
---
+++ src/Lol.Home/UI/Views/AvatarView.cs
@@ -0,0 +1,29 @@
+﻿using Lol.LayoutSupport.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lol.Home.UI.Views
+{
+    public class AvatarView : View
+    {
+        #region DefaultStyleKey
+
+        static AvatarView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AvatarView), new FrameworkPropertyMetadata(typeof(AvatarView)));
+        }
+        #endregion
+    }
+}
