a494a31a38fa83e4fe12d7f8ff16905a03e7f7ec
devncore-elena <elena.kim@devncore.org>
2021-12-02 22:56:13 +0900

    Update CreateCustomView

--- src/Lol.GameRoom/Local/ViewModels/CreateCustomViewModel.cs
+++ src/Lol.GameRoom/Local/ViewModels/CreateCustomViewModel.cs
@@ -15,6 +15,8 @@ namespace Lol.GameRoom.Local.ViewModels
         private GameRoomsCustom _currentCustom;
         private List<UserCount> _userCount;
         private UserCount _currentUserCount;
+        private List<CodeItems> _customGameType;
+        private List<CodeItems> _spectatorMode;
 
         public List<GameRoomsCustom> Customs { get; set; }
         #endregion
@@ -51,17 +53,39 @@ namespace Lol.GameRoom.Local.ViewModels
         }
         #endregion
 
+        #region CustomGameType
+
+        public List<CodeItems> CustomGameType
+        {
+            get => _customGameType;
+            set { _customGameType = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region SpectatorMode
+
+        public List<CodeItems> SpectatorMode
+        {
+            get => _spectatorMode;
+            set { _spectatorMode = value; OnPropertyChanged(); }
+        }
+        #endregion
+
         #region Constructor
 
         public CreateCustomViewModel(Action<object> customConfirm)
         {
             _customConfirm = customConfirm;
+
             Customs = new GameRoomApi().GetGameRoomsCustom();
+            CurrentCustom = Customs.FirstOrDefault();
 
             UserCount = new GameRoomApi().GetUserCount();
             CurrentUserCount = UserCount.LastOrDefault();
 
-            CurrentCustom = Customs.FirstOrDefault();
+            CustomGameType = GetCustomGameType();
+            SpectatorMode = GetSpectatorMode();
+
             ConfirmCommand = new RelayCommand<object>(CreateRoom);
         }
         #endregion
@@ -73,5 +97,31 @@ namespace Lol.GameRoom.Local.ViewModels
             _customConfirm.Invoke(obj);
         }
         #endregion
+
+        // 임시 작업
+        private List<CodeItems> GetCustomGameType()
+        {
+            List<CodeItems> source = new()
+            {
+                new CodeItems { Seq = 0, ItemName = "비공개 선택" },
+                new CodeItems { Seq = 1, ItemName = "교차 선택" },
+                new CodeItems { Seq = 2, ItemName = "모두 무작위" },
+                new CodeItems { Seq = 3, ItemName = "토너먼트 드래프트" }
+            };
+            return source;
+        }
+
+        private List<CodeItems> GetSpectatorMode()
+        {
+            List<CodeItems> source = new()
+            {
+                new CodeItems { Seq = 0, ItemName = "로비 관전자 공개" },
+                new CodeItems { Seq = 1, ItemName = "친구 공개" },
+                new CodeItems { Seq = 2, ItemName = "전체 공개" },
+                new CodeItems { Seq = 3, ItemName = "비공개" }
+            };
+            return source;
+        }
+
     }
 }
