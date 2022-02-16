1d413c4c493d7cd073f40681031c0de57026e7e5
devncore-elena <elena.kim@devncore.org>
2022-02-16 13:55:03 +0900

    Update Profile Hightlight View

---
+++ src/Lol.Profile/UI/Units/Summary/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Profile.UI.Units
+{
+    public class UserProfile : Control
+    {
+        #region DefaultStyleKey
+
+        static UserProfile()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(UserProfile), new FrameworkPropertyMetadata(typeof(UserProfile)));
+        }
+        #endregion
+    }
+}
