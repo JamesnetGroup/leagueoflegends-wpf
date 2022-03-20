6b1ef411981f26e56d77c645309013f9a7a590c2
james.lee <james.lee@devncore.org>
2022-03-20 23:21:57 +0900

    Update solution

---
+++ src/PreviewDesign/PrevMainView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Lol.Main.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PreviewDesign
+{
+    /// <summary>
+    /// PrevMainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PrevMainView : UserControl
+    {
+        public PrevMainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
