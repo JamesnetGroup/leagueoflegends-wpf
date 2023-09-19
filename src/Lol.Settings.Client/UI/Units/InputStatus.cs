b9bd8397e1377855d11e4972439d69e369bf73c6
lukewire <lukewire129@gmail.com>
2023-09-19 18:35:40 +0900

    Voice 부분 작업 진행 중..

--- src/Lol.Settings.Client/UI/Units/InputStatus.cs
+++ src/Lol.Settings.Client/UI/Units/InputStatus.cs
@@ -5,7 +5,6 @@ namespace Lol.Settings.Client.UI.Units
 {
     public class InputStatus : ContentControl
     {
-
         static InputStatus()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(InputStatus), new FrameworkPropertyMetadata(typeof(InputStatus)));
---
+++ src/Lol.Settings.Client/UI/Units/Voice/InputModeListBoxItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lol.Settings.Client.UI.Units.Voice
+{
+    public class InputModeListBoxItem : ListBoxItem
+    {
+        static InputModeListBoxItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(InputModeListBoxItem), new FrameworkPropertyMetadata(typeof(InputModeListBoxItem)));
+        }
+    }
+}
