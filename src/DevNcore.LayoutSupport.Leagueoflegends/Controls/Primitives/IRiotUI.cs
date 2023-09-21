a33871608ce99c28a29889ba7d8d4182929ea0f2
JAEWUNG LEE <james@jamesnet.dev>
2023-09-22 04:31:55 +0900

    Updates

--- src/DevNcore.LayoutSupport.Leagueoflegends/Controls/Primitives/IRiotUI.cs
+++
@@ -1,9 +0,0 @@
-﻿using DevNcore.UI.Foundation.Mvvm;
-
-namespace DevNcore.LayoutSupport.Leagueoflegends.Controls.Primitives
-{
-    public interface IRiotUI
-    {
-        IRiotUI SetVM(ObservableObject vm);
-    }
-}
