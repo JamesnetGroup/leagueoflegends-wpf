4cef0f9f7115cb622dc9d566598ad1c08a37fa8e
lukewire129 <lukewire129@gmail.com>
2023-09-22 10:09:36 +0900

    왜안나오지 .. 왜..!

--- src/Lol.Settings.Client/UI/Units/Voice/InputModeListBoxItem.cs
+++ src/Lol.Settings.Client/UI/Units/Voice/InputModeListBoxItem.cs
@@ -1,13 +1,34 @@
-﻿using System.Windows;
+﻿using Lol.Database.Entites.Schema;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Lol.Settings.Client.UI.Units.Voice
 {
-    public class InputModeListBoxItem : ListBoxItem
+    public class InputModeListBoxItem : RadioButton
     {
+        public ICommand ChangeVoiceModeCommand
+        {
+            get { return (ICommand)GetValue (ChangeVoiceModeCommandProperty); }
+            set { SetValue (ChangeVoiceModeCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for VoiceChangeCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ChangeVoiceModeCommandProperty =
+            DependencyProperty.Register ("ChangeVoiceModeCommand", typeof (ICommand), typeof (InputModeListBoxItem), new PropertyMetadata (null));
+
+
         static InputModeListBoxItem()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(InputModeListBoxItem), new FrameworkPropertyMetadata(typeof(InputModeListBoxItem)));
+        }
+
+        public InputModeListBoxItem()
+        {
+            Checked += (sender, args) =>
+            {
+                ChangeVoiceModeCommand?.Execute (this.DataContext as UsualCodes);
+            };
         }
     }
 }
