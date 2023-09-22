2cdd22e185f4ea30b0a6737371947d3b7e87aa2d
lukewire129 <lukewire129@gmail.com>
2023-09-22 23:33:16 +0900

    오디오 입력 어떻게하지..

---
+++ src/Lol.Settings.Client/UI/Units/OutputModeComboBoxItem.cs
@@ -0,0 +1,29 @@
+﻿using DevNcore.LayoutSupport.Leagueoflegends.Controls;
+using Lol.Data.Codes;
+using Lol.Database.Entites.Schema;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lol.Settings.Client.UI.Units
+{
+    public class OutputModeComboBoxItem : RiotComboBoxItem
+    {
+        public ICommand ChangeOutputModeCommand
+        {
+            get { return (ICommand)GetValue (ChangeOutputModeCommandProperty); }
+            set { SetValue (ChangeOutputModeCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for VoiceChangeCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ChangeOutputModeCommandProperty =
+            DependencyProperty.Register ("ChangeOutputModeCommand", typeof (ICommand), typeof (RiotComboBoxItem), new PropertyMetadata (null));
+
+        public OutputModeComboBoxItem()
+        {
+            this.Selected += ((s, e) =>
+            {
+                this.ChangeOutputModeCommand?.Execute (this.DataContext as AudioDevice);
+            });
+        }
+    }
+}
